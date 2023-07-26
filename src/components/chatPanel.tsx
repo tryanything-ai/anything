15a4b03d9406c8bcf221a9504f81b7ae647a159b
Carl Lippert <carl.lippert@gmail.com>
2023-07-26 12:43:32 -0700

    new sqllite db with more usefull events table

--- src/components/chatPanel.tsx
+++ src/components/chatPanel.tsx
@@ -1,7 +1,14 @@
+import { useSqlContext } from "../context/SqlProvider";
+
 const ChatPanel = () => {
+  const { addEvent } = useSqlContext();
+
   return (
     <div className="flex flex-col h-full p-4 border-l border-gray-500">
-      <button className="btn btn-neutral">CHAT</button>
+      <button className="btn btn-neutral">Chat</button>
+      <button onClick={addEvent} className="btn btn-neutral">
+        Add Event
+      </button>
     </div>
   );
 };
--- src/context/SqlProvider.tsx
+++ src/context/SqlProvider.tsx
@@ -11,7 +11,16 @@ const db = await Database.load("sqlite:test.db");
 
 interface SqlContextInterface {
   tables: any[];
-  addEvent: (name: string) => void;
+  addEvent: (
+    event_id: string,
+    flow_id: string,
+    flow_name: string,
+    flow_version: string,
+    stage: string,
+    status: string,
+    created_at: string,
+    data: any
+  ) => void;
   getTableData: (tableName: string) => any;
 }
 
@@ -26,14 +35,35 @@ export const useSqlContext = () => useContext(SqlContext);
 export const SqlProvider = ({ children }: { children: ReactNode }) => {
   const [tables, setTables] = useState<any[]>([]);
 
-  const addEvent = async (name: string) => {
-    await db.execute("INSERT INTO events (name) VALUES (?)", [name]);
+  const addEvent = async (
+    event_id: string,
+    flow_id: string,
+    flow_name: string,
+    flow_version: string,
+    stage: string,
+    status: string,
+    created_at: string,
+    data: any
+  ) => {
+    await db.execute(
+      "INSERT INTO events (event_id, flow_id, flow_name, flow_version, stage, status, created_at, data) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
+      [
+        event_id,
+        flow_id,
+        flow_name,
+        flow_version,
+        stage,
+        status,
+        created_at,
+        data,
+      ]
+    );
   };
 
   const getTables = async () => {
     const tables = await db.select(
       `SELECT name 
-      FROM sqlite_master 
+      FROM sqlite_master
       WHERE type='table' AND name NOT LIKE 'sqlite_%'
       `
     );
@@ -45,13 +75,23 @@ export const SqlProvider = ({ children }: { children: ReactNode }) => {
     const tableData = await db.select(`SELECT * FROM ${tableName}`);
     console.log("tableData in db", tableData);
     return tableData;
-    // setTables(tableData as any);
   };
 
   const initDb = async () => {
-    await db.execute(
-      "CREATE TABLE IF NOT EXISTS events (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL)"
-    );
+    try {
+      await db.execute(`CREATE TABLE IF NOT EXISTS events (
+      event_id TEXT PRIMARY KEY,
+      flow_id TEXT, 
+      flow_name TEXT,
+      flow_version TEXT,
+      stage TEXT,
+      status TEXT,
+      created_at DATETIME,
+      data TEXT
+      )`);
+    } catch (error) {
+      console.log("error creating events table", error);
+    }
   };
 
   useEffect(() => {
