b5840c49359c28ed94a3cafbafe2a854f5fbe1b6
Carl Lippert <carl.lippert@gmail.com>
2023-07-27 15:35:40 -0700

    add node works basic

--- src/components/chatPanel.tsx
+++ src/components/chatPanel.tsx
@@ -1,12 +1,26 @@
+import { randomUUID } from "crypto";
 import { useSqlContext } from "../context/SqlProvider";
 
 const ChatPanel = () => {
   const { addEvent } = useSqlContext();
 
+  const createMockEvent = () => {
+    addEvent(
+      randomUUID(),
+      "flow_id",
+      "flow_name",
+      "flow_version",
+      "stage",
+      "status",
+      "created_at",
+      "data"
+    );
+  };
+
   return (
     <div className="flex flex-col h-full p-4 border-l border-gray-500">
       <button className="btn btn-neutral">Chat</button>
-      <button onClick={addEvent} className="btn btn-neutral">
+      <button onClick={createMockEvent} className="btn btn-neutral">
         Add Event
       </button>
     </div>
--- src/components/nodePanel.tsx
+++ src/components/nodePanel.tsx
@@ -1,44 +1,11 @@
+import { useFlowContext } from "../context/FlowProvider";
+
 const NodePanel = () => {
-  function findNextNodeId(nodes: any): string {
-    // Initialize the maxId to 0
-    let maxId = 0;
-
-    console.log("nodes in FindNextNodeId", nodes);
-
-    // Loop through the nodes and find the maximum numeric ID value
-    nodes.forEach((node: any) => {
-      const numericId = parseInt(node.id, 10);
-      console.log("numericId", numericId);
-      if (!isNaN(numericId) && numericId > maxId) {
-        maxId = numericId;
-      }
-    });
-
-    // Increment the maxId to get the next ID for the new node
-    const nextId = (maxId + 1).toString();
-
-    return nextId;
-  }
-
-  const addNode = () => {
-    // const id = findNextNodeId(nodes);
-    // const newNode = {
-    //   id,
-    //   position: {
-    //     x: Math.random() * 500,
-    //     y: Math.random() * 500,
-    //   },
-    //   data: {
-    //     label: `Node ${id}`,
-    //   },
-    // };
-    // reactFlowInstance.addNodes(newNode); //TODO: fix this
-    console.log("addNode");
-  };
+  const { addNode } = useFlowContext();
 
   return (
     <div className="flex flex-col h-full p-4 border-l border-gray-500">
-      <button onClick={addNode} className="btn btn-neutral">
+      <button onClick={() => addNode("default")} className="btn btn-neutral">
         Add Node
       </button>
     </div>
