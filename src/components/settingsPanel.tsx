319aac877d23d60f7fffda30bcff8a25b168e4e4
Carl Lippert <carl.lippert@gmail.com>
2023-08-14 16:09:25 -0700

    added some basic protections and a kinda buggy rename flow feature

--- src/components/settingsPanel.tsx
+++ src/components/settingsPanel.tsx
@@ -2,15 +2,15 @@ import { useEffect, useState } from "react";
 import { useFlowContext } from "../context/FlowProvider";
 import { useLocalFileContext } from "../context/LocalFileProvider";
 import { useForm, SubmitHandler } from "react-hook-form";
-import { useParams, useNavigate} from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 type Inputs = {
-  example: string;
-  exampleRequired: string;
+  flow_name: string;
 };
 
 const FlowSettingsPanel = () => {
-  const { deleteFlow } = useLocalFileContext();
+  const [loading, setLoading] = useState(false);
+  const { deleteFlow, renameFlow } = useLocalFileContext();
   const { flow_name } = useParams();
   const navigate = useNavigate();
   const {
@@ -20,25 +20,50 @@ const FlowSettingsPanel = () => {
     formState: { errors },
   } = useForm<Inputs>();
 
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    setLoading(true);
+    if (flow_name && data.flow_name != flow_name) {
+      renameFlow(flow_name, data.flow_name);
+      //wait for 2 seconds
+      setTimeout(() => {
+        navigate(`/flows/${data.flow_name}`);
+        setLoading(false);
+      }, 2000);
+    }
+    console.log(data);
+    setLoading(false);
+  };
 
   // console.log(watch("example")); // watch input value
   return (
-    <div className="flex flex-col h-full p-4 border-l border-gray-500">
-      <h1 className="text-2xl font-bold">Setttings Form</h1>
+    <div className="flex flex-col h-full p-4 border-l border-gray-500 gap-5">
+      <h1 className="text-2xl font-bold">Flow Settings</h1>
 
-      <form onSubmit={handleSubmit(onSubmit)} className="flex-grow">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col flex-grow gap-5"
+      >
+        <input
+          type="text"
+          placeholder="Type here"
+          className="input input-bordered input-md w-full"
+          defaultValue={flow_name}
+          {...register("flow_name", { required: true })}
+        />
+
         {/* register your input into the hook by invoking the "register" function */}
-        <input defaultValue="test" {...register("example")} />
-        {/* include validation with required or other standard HTML validation rules */}
-        <input {...register("exampleRequired", { required: true })} />
-        {/* errors will return when field validation fails  */}
-        {errors.exampleRequired && <span>This field is required</span>}
-        <input type="submit" />
+        {/* <input defaultValue={flow_name} {...register("flow_name", { required: true})} /> */}
+        {errors.flow_name && <span>This field is required</span>}
+        <input type="submit" className="btn btn-primary" />
       </form>
       <button
         className="btn btn-error mt-4"
-        onClick={() => { if (flow_name) { deleteFlow(flow_name); navigate("/")} }}
+        onClick={() => {
+          if (flow_name) {
+            deleteFlow(flow_name);
+            navigate("/");
+          }
+        }}
       >
         Delete Flow
       </button>
