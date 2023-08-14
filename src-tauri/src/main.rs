88821f2e56f08498de652c745a672529511ad81d
Carl Lippert <carl.lippert@gmail.com>
2023-08-13 19:46:11 -0700

    cleanup

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,27 +1,27 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::{thread, time::Duration};
-
 mod sql;
-mod events;
-mod events2; 
+mod events2;
 
-// use sql::plugin::Builder; 
+use sql::plugin::Builder; 
 use events2::scheduler; 
 
-#[tokio::main]
-async fn main() {
-    tauri::async_runtime::set(tokio::runtime::Handle::current());
+fn main() {
     
     tauri::Builder::default()
         .plugin(tauri_plugin_fs_watch::init())
-        .setup(|_app| {
-            tokio::task::spawn(scheduler()); 
+        .plugin(Builder::default().build())
+        .setup(|app| {
+
+            let app_handle = app.handle();
+              // Spawn a new asynchronous task for scheduler
+              tauri::async_runtime::spawn(async move {
+                scheduler(&app_handle).await;
+            });
+
             Ok(())
         })
         .run(tauri::generate_context!())    
         .expect("error while running tauri application");
 }
-
-
