1513108728e7743ce4921fd4503f883b0c1c525e
Carl Lippert <carl.lippert@gmail.com>
2023-08-15 20:36:41 -0700

    this totally fails. might try dif implementation

--- src-tauri/src/rustformers/cancellation.rs
+++ src-tauri/src/rustformers/cancellation.rs
@@ -3,6 +3,7 @@ use std::convert::Infallible;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Mutex;
 use tracing::info;
+use llm::InferenceFeedback;
 
 pub struct Canceller {
     cancelled: AtomicBool,
@@ -27,7 +28,7 @@ impl Canceller {
     }
 
     #[tracing::instrument(skip(self))]
-    pub fn inference_feedback(&self) -> Result<llm::InferenceFeedback, Infallible> {
+    pub fn inference_feedback(&self) -> Result<InferenceFeedback, Infallible> {
         // When a cancellation occurs, the sender will block until it is received at least
         // once. We want to check and see if that message has been sent, and if so we'll cancel.
         let cancelled = if let Ok(rx) = self.rx.try_lock() {
@@ -37,9 +38,9 @@ impl Canceller {
         };
         if cancelled || self.is_cancelled() {
             info!("sending halt");
-            Ok(llm::InferenceFeedback::Halt)
+            Ok(InferenceFeedback::Halt)
         } else {
-            Ok(llm::InferenceFeedback::Continue)
+            Ok(InferenceFeedback::Continue)
         }
     }
 }
--- src-tauri/src/rustformers/models.rs
+++ src-tauri/src/rustformers/models.rs
@@ -2,6 +2,7 @@ use lazy_static::lazy_static;
 use serde::{Deserialize, Serialize};
 use anyhow::Result;
 use std::fs;
+use std::fs::create_dir_all;
 
 use crate::rustformers::prompt::Template;
 use crate::rustformers::config::get_models_dir;
@@ -121,4 +122,75 @@ pub struct ModelManager {
     pub model: Box<dyn llm::Model>,
     pub session: llm::InferenceSession,
     pub template: Template,
+}
+
+impl ModelManager {
+    pub fn infer<F>(&mut self, prompt: &str, callback: F) -> Result<llm::InferenceStats, String>
+    where
+        F: FnMut(llm::InferenceResponse) -> Result<llm::InferenceFeedback, Infallible>,
+    {
+        self.session
+            .infer(
+                self.model.as_ref(),
+                &mut rand::thread_rng(),
+                &llm::InferenceRequest {
+                    prompt: self.template.process(prompt).as_str().into(),
+                    parameters: &llm::InferenceParameters::default(),
+                    play_back_previous_tokens: false,
+                    maximum_token_count: None,
+                },
+                &mut Default::default(),
+                callback,
+            )
+            .map_err(|e| format!("Error inferring: {}", e))
+    }
+}
+
+#[tracing::instrument(skip(progress))]
+async fn download_file<F>(
+    url: &str,
+    destination: &PathBuf,
+    filename: &str,
+    progress: F,
+) -> Result<PathBuf>
+where
+    F: Fn(u64, u64, f32),
+{
+    create_dir_all(destination)?;
+    let destination = destination.join(filename);
+    let destination = destination.to_str().unwrap();
+    info!("downloading model to {}", destination);
+    let response = reqwest::get(url).await?;
+    let total_size = response.content_length().unwrap_or(0);
+    let mut stream = response.bytes_stream();
+    let mut file = std::fs::File::create(destination)?;
+    let mut downloaded: u64 = 0;
+    while let Some(item) = stream.next().await {
+        let chunk = item?;
+        file.write_all(&chunk)?;
+        let new_downloaded = min(downloaded + chunk.len() as u64, total_size);
+        if total_size > 0 {
+            let p = new_downloaded as f32 / total_size as f32;
+            progress(downloaded, total_size, p);
+        }
+        downloaded = new_downloaded;
+    }
+    Ok(PathBuf::from(destination))
+}
+
+#[tracing::instrument(skip(progress))]
+pub async fn get_local_model<F>(filename: &str, progress: F) -> Result<PathBuf>
+where
+    F: Fn(u64, u64, f32),
+{
+    let models_dir = get_models_dir()?;
+    if !models_dir.join(filename).exists() {
+        let model = AVAILABLE_MODELS
+            .iter()
+            .find(|m| m.filename == filename)
+            .ok_or(anyhow::anyhow!("Model not found"))?;
+        download_file(&model.url, &models_dir, &model.filename, progress).await?;
+        info!(filename = model.filename, "finished downloading model");
+    }
+    Ok(models_dir.join(filename))
 }
