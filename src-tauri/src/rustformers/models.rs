5f70e7a528fe79cad59ab9962d55f009a9ecd55c
Carl Lippert <carl.lippert@gmail.com>
2023-08-15 17:01:17 -0700

    got architectures coming into js

--- src-tauri/src/rustformers/models.rs
+++ src-tauri/src/rustformers/models.rs
@@ -1,3 +1,48 @@
+use lazy_static::lazy_static;
+use serde::{Deserialize, Serialize};
+use anyhow::Result;
+use std::fs;
+
+use crate::rustformers::prompt::Template;
+use crate::rustformers::config::get_models_dir;
+
+lazy_static! {
+    pub static ref AVAILABLE_MODELS: Vec<Model> =
+        serde_json::from_str(include_str!("./data/models.json")).unwrap();
+    pub static ref AVAILABLE_ARCHITECTURES: Vec<Architecture> = vec![
+        Architecture {
+            name: "LLaMA".to_string(),
+            id: "llama".to_string(),
+            inner: llm::ModelArchitecture::Llama,
+        },
+        Architecture {
+            name: "GPT-2".to_string(),
+            id: "gpt-2".to_string(),
+            inner: llm::ModelArchitecture::Gpt2,
+        },
+        Architecture {
+            name: "GPT-J".to_string(),
+            id: "gpt-j".to_string(),
+            inner: llm::ModelArchitecture::GptJ,
+        },
+        // Architecture { //TODO: something is unhappy here
+        //     name: "GPT-NeoX".to_string(),
+        //     id: "gpt-neo-x".to_string(),
+        //     inner: llm::ModelArchitecture::GptNeoX,
+        // },
+        // Architecture {
+        //     name: "MPT".to_string(),
+        //     id: "mpt".to_string(),
+        //     inner: llm::ModelArchitecture::Mpt,
+        // },
+        Architecture {
+            name: "BLOOM".to_string(),
+            id: "bloom".to_string(),
+            inner: llm::ModelArchitecture::Bloom,
+        },
+    ];
+}
+
 /// Returns a list of all .bin files available in the models directory
 /// (with associated metadata if we have them in our models.json file)
 /// and if the model is a model that we don't know about, then we return
@@ -27,4 +72,53 @@ pub async fn get_available_models() -> Result<Vec<Model>> {
     models.append(&mut known_models);
     models.sort_by(|a, b| b.custom.cmp(&a.custom));
     Ok(models)
+}
+
+
+#[derive(Serialize, Deserialize, Clone, Default)]
+#[serde(rename_all = "camelCase")]
+pub struct Model {
+    name: String,
+    url: String,
+    #[serde(default)]
+    pub custom: bool,
+    #[serde(default)]
+    pub recommended: bool,
+    pub filename: String,
+    pub description: String,
+    pub quantization: Option<Quantization>,
+    pub parameter_count: Option<String>,
+    pub labels: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Default)]
+pub enum Quantization {
+    #[serde(rename = "none")]
+    #[default]
+    None,
+    #[serde(rename = "8-bit")]
+    Bit8,
+    #[serde(rename = "6-bit")]
+    Bit6,
+    #[serde(rename = "5-bit")]
+    Bit5,
+    #[serde(rename = "4-bit")]
+    Bit4,
+    #[serde(rename = "2-bit")]
+    Bit2,
+}
+
+// #[derive(Serialize, Clone)] didnt work cause modelArchitecture does not implement seriealize
+#[derive(Serialize, Clone)]
+pub struct Architecture {
+    name: String,
+    pub id: String,
+    #[serde(skip_serializing)] ///carl added cause LLM does not implement serialize and made everything angry
+    pub inner: llm::ModelArchitecture,
+}
+
+pub struct ModelManager {
+    pub model: Box<dyn llm::Model>,
+    pub session: llm::InferenceSession,
+    pub template: Template,
 }
