eb4d2b1b200beb1d2b52d131e7015e720456a5bc
Carl Lippert <carl.lippert@gmail.com>
2023-08-15 17:01:17 -0700

    got architectures coming into js

--- src-tauri/src/rustformers/mod.rs
+++ src-tauri/src/rustformers/mod.rs
@@ -1,7 +1,12 @@
 extern crate llm;
 
+pub mod config; 
 pub mod prompt;
+
 use prompt::Template; 
+
+pub mod models;
+use models::Architecture; 
 
 use tauri::{
     plugin::{Builder, TauriPlugin},
@@ -12,9 +17,14 @@ use tauri::{
 fn get_prompt_templates() -> Vec<Template> {
     prompt::AVAILABLE_TEMPLATES.clone()
 }
+
+#[tauri::command]
+fn get_architectures() -> Vec<Architecture> {
+    models::AVAILABLE_ARCHITECTURES.clone()
+}
   
 pub fn init<R: Runtime>() -> TauriPlugin<R> {
     Builder::new("rustformers")
-      .invoke_handler(tauri::generate_handler![get_prompt_templates])
+      .invoke_handler(tauri::generate_handler![get_prompt_templates, get_architectures])
       .build()
   }
--- src/context/ModelsProvider.tsx
+++ src/context/ModelsProvider.tsx
@@ -1,47 +1,56 @@
 import React, {
-    createContext,
-    useState,
-    useEffect,
-    useContext,
-    ReactNode,
+  createContext,
+  useState,
+  useEffect,
+  useContext,
+  ReactNode,
 } from "react";
-  
+
 import { invoke } from "@tauri-apps/api";
 
 type ModelPromptTemplate = {
-name: string;
-warmup: string;
-template: string;
+  name: string;
+  warmup: string;
+  template: string;
 };
-
 
 interface ModelContextInterface {
   models: any[];
+  architectures: any[];
   modelPromptTemplates: ModelPromptTemplate[];
 }
+
+export const ModelContext = createContext<ModelContextInterface>({
+  models: [],
+  architectures: [],
+  modelPromptTemplates: [],
+});
+
+export const useModelContext = () => useContext(ModelContext);
+
+export const ModelProvider = ({ children }: { children: ReactNode }) => {
+  const [models, setModels] = useState<string[]>([]);
+  const [modelPromptTemplates, setModelPromptTemplates] = useState<
+    ModelPromptTemplate[]
+    >([]);
   
-  export const ModelContext = createContext<ModelContextInterface>({
-    models: [],
-    modelPromptTemplates: [],
-  });
-  
-  export const useModelContext = () => useContext(ModelContext);
-  
-  export const ModelProvider = ({ children }: { children: ReactNode }) => {
-    const [models, setModels] = useState<string[]>([]);
-    const [modelPromptTemplates, setModelPromptTemplates] = useState<ModelPromptTemplate[]>([]);   
-      
-    useEffect(() => {
-      invoke("plugin:rustformers|get_prompt_templates").then((result) => {
-        console.log("Prompt Templates from plugin" +JSON.stringify(result));
-        setModelPromptTemplates(result as ModelPromptTemplate[]);
-      });
-    }, []);
-      
-    return (
-      <ModelContext.Provider value={{ models, modelPromptTemplates }}>
-        {children}
-      </ModelContext.Provider>
-    );
-  };
-  
+  const [architectures, setArchitectures] = useState<any[]>([]);
+
+  useEffect(() => {
+    invoke("plugin:rustformers|get_prompt_templates").then((result) => {
+      console.log("Prompt Templates from plugin" + JSON.stringify(result));
+      setModelPromptTemplates(result as ModelPromptTemplate[]);
+    });
+
+    invoke("plugin:rustformers|get_architectures").then((result) => {
+      console.log("Architectures from plugin" + JSON.stringify(result));
+      setArchitectures(result as any[]);
+    });
+  }, []);
+
+  return (
+    <ModelContext.Provider value={{ models, modelPromptTemplates, architectures }}>
+      {children}
+    </ModelContext.Provider>
+  );
+};
