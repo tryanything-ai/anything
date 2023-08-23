161b4adc54712195f1d8fe02dfa71333a7415f66
Carl Lippert <carl.lippert@gmail.com>
2023-08-23 14:19:39 -0700

    running prompt basically crashed my computer lol

--- src/routes/models.tsx
+++ src/routes/models.tsx
@@ -4,76 +4,28 @@ import { useModelContext } from "../context/ModelsProvider";
 import { invoke } from "@tauri-apps/api";
 import { useEffect } from "react";
 import { useEventLoopContext } from "../context/EventLoopProvider";
+import ModelCard from "../components/downloadedModelCard";
 
 export default function Models() {
-  const { subscribeToEvent } = useEventLoopContext();
+  const { models, downloadModel, downloadedModels } = useModelContext();
 
-  useEffect(() => {
-    subscribeToEvent("model_loading", (event: any) => {
-      console.log("model_download_progress event received");
-      console.log(event);
-    });
-  }, []);
-
-  const {
-    models,
-    downloadModel,
-    downloadedModels,
-    architectures,
-    modelPromptTemplates,
-  } = useModelContext();
   return (
-    <div className="flex w-full p-10 h-screen overflow-y-auto">
+    <div className="flex flex-col w-full p-10 h-screen overflow-y-auto">
       {/* Downloaded Models */}
-      <div className="flex flex-col text-5xl text-primary-content m-5 w-full">
-        <div className="flex flex-row justify-between">
-          <div>Downloaded Models</div>
+      <div className="flex flex-col text-5xl text-primary-content w-full">
+        <div className="flex flex-col">
+          <div className="pl-4">Downloaded Models</div>
         </div>
         <ul className="mt-4">
           {downloadedModels.map((model) => {
-            return (
-              <div
-                key={model.filename}
-                className="card w-full bg-base-300 shadow-xl my-2"
-              >
-                <div className="card-body flex-row justify-between">
-                  <div className="w-1/4">
-                    <div className="text-2xl">{model.name}</div>
-                    <div className="text-sm">{model.description}</div>
-                  </div>
-                  <div className="flex text-lg">{model.parameterCount}</div>
-                  <div className="flex text-lg">{model.quantization}</div>
-                  <button
-                    className="btn btn-neutral text-lg"
-                    onClick={() => {
-                      invoke("start", {
-                        modelFilename: model.filename,
-                        architecture: architectures[0].id,
-                        tokenizer: "embedded",
-                        contextSize: 2048,
-                        useGpu: true,
-                        prompt: modelPromptTemplates[0],
-                        contextFiles: [],
-                      });
-                    }}
-                  >
-                    Start
-                  </button>
-                  {/* <h2 className="card-title">{flow.name}</h2>
-                  <div className="card-actions justify-end">
-                    <div className="bg-pink-200 h-full w-full">derp</div>
-                  </div> */}
-                </div>
-                {/* </Link> */}
-              </div>
-            );
+            return <ModelCard model={model} />;
           })}
         </ul>
       </div>
       {/* All Models */}
-      <div className="flex flex-col text-5xl text-primary-content m-5 w-full">
-        <div className="flex flex-row justify-between">
-          <div>All Models</div>
+      <div className="flex flex-col text-5xl text-primary-content w-full mt-4">
+        <div className="flex flex-col justify-between">
+          <div className="pl-4">All Models</div>
         </div>
         <ul className="mt-4">
           {models.map((model) => {
