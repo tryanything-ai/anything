9709f2dc3ac86439a1589e348e5b1cfb8e81c7b9
Carl Lippert <carl.lippert@gmail.com>
2023-08-27 15:23:45 -0700

    simpler loading state

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      animation: {
-        border: 'border 4s ease infinite',
-      },
-      keyframes: {
-        border: {
-          '0%, 100%': { backgroundPosition: '0% 50%' },
-          '50%': { backgroundPosition: '100% 50%' },
-        },
-      },
-    },
+    extend: {},
   },
   daisyui: {
     themes: [
