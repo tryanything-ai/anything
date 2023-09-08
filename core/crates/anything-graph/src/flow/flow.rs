c8538c5e890cd1cec83ebf0ed2fc2ff66f45de6c
Ari <me@ari.io>
2023-09-07 17:39:18 -0700

    Updated steps => nodes

--- core/crates/anything-graph/src/flow/flow.rs
+++ core/crates/anything-graph/src/flow/flow.rs
@@ -6,7 +6,7 @@ use crate::error::{AppError, AppResult};
 
 use super::{
     action::Action,
-    node::{Node, NodeBuilder, StepList},
+    node::{Node, NodeBuilder, NodeList},
     sequencer::{find_node_recursive, get_nodes_in_order},
     trigger::Trigger,
 };
@@ -24,10 +24,10 @@ pub struct Flow {
 impl Flow {
     pub fn new() -> Self {
         let mut new_dag = Dag::<Node, ()>::new();
-        let mut root_step = Node::default();
-        root_step.name = "root_step".to_string();
+        let mut root_node = Node::default();
+        root_node.name = "root_node".to_string();
 
-        let parent = new_dag.add_node(root_step);
+        let parent = new_dag.add_node(root_node);
         Self {
             id: String::default(),
             name: String::default(),
@@ -39,62 +39,62 @@ impl Flow {
     }
 
     pub fn as_dotfile(&self, start_from: Option<String>) -> String {
-        let steps = if let Some(start_step) = start_from {
-            self.get_nodes_in_order_from(&start_step)
+        let nodes = if let Some(start_node) = start_from {
+            self.get_nodes_in_order_from(&start_node)
         } else {
             self.get_nodes_in_order()
         };
 
-        let mut topological_sorted_steps = vec![];
+        let mut topological_sorted_nodes = vec![];
 
-        for step in steps.iter() {
-            for s in step.iter() {
+        for node in nodes.iter() {
+            for s in node.iter() {
                 for dep in s.iter() {
                     println!("{} -> {}", dep.name, dep.name);
-                    topological_sorted_steps.push(dep);
+                    topological_sorted_nodes.push(dep);
                 }
             }
         }
 
         let title = format!("digraph {} {{", self.name);
-        let step_names = topological_sorted_steps
+        let node_names = topological_sorted_nodes
             .iter()
             .map(|s| format!("  \"{}\"\n", s.name))
             .collect::<String>();
 
-        let step_connections = topological_sorted_steps
+        let node_connections = topological_sorted_nodes
             .iter()
-            .map(|step| {
-                step.depends_on
+            .map(|node| {
+                node.depends_on
                     .iter()
-                    .map(|dep| format!("  \"{}\" -> \"{}\"\n", step.name, dep))
+                    .map(|dep| format!("  \"{}\" -> \"{}\"\n", node.name, dep))
                     .collect::<String>()
             })
             .collect::<String>();
 
-        format!("{}\n{}{}{}", title, step_names, step_connections, "}")
+        format!("{}\n{}{}{}", title, node_names, node_connections, "}")
     }
 
     #[cfg(test)]
-    pub fn add_step_obj(&mut self, step: &Node) -> AppResult<bool> {
-        let depends_on = step
+    pub fn add_node_obj(&mut self, node: &Node) -> AppResult<bool> {
+        let depends_on = node
             .depends_on
             .iter()
             .map(AsRef::as_ref)
             .collect::<Vec<&str>>();
 
-        self.add_step(&step.name, &step.step_action, &depends_on)
+        self.add_node(&node.name, &node.node_action, &depends_on)
     }
 
-    pub fn add_step(
+    pub fn add_node(
         &mut self,
         name: &str,
         action: &Action,
         depends_on: &Vec<&str>,
     ) -> AppResult<bool> {
-        if let Some((_, step)) = self.find_node_by_name(name) {
+        if let Some((_, node)) = self.find_node_by_name(name) {
             return Err(AppError::FlowError(
-                format!("Step {} already exists", step.name).to_string(),
+                format!("Node {} already exists", node.name).to_string(),
             ));
         }
 
@@ -102,7 +102,7 @@ impl Flow {
             // Add dependencies
             if depends_on.iter().any(|s| s == &name) {
                 return Err(AppError::FlowError(format!(
-                    "A step cannot depend on itself"
+                    "A node cannot depend on itself"
                 )));
             }
 
@@ -115,15 +115,15 @@ impl Flow {
                     deps.push(dependency.to_string());
                 } else {
                     return Err(AppError::FlowError(format!(
-                        "A step must have dependencies defined already. Could not find step: {}",
+                        "A node must have dependencies defined already. Could not find node: {}",
                         dependency
                     )));
                 }
             }
 
-            let step = NodeBuilder::default()
+            let node = NodeBuilder::default()
                 .name(name.to_string())
-                .step_action(action.clone())
+                .node_action(action.clone())
                 .depends_on(
                     depends_on
                         .iter()
@@ -132,7 +132,7 @@ impl Flow {
                 )
                 .build()?;
 
-            let node = self.dag.add_node(step);
+            let node = self.dag.add_node(node);
 
             for parent in parents {
                 if let Err(_) = self.dag.add_edge(parent, node, ()) {
@@ -143,70 +143,70 @@ impl Flow {
                 }
             }
         } else {
-            let step = NodeBuilder::default()
+            let node = NodeBuilder::default()
                 .name(name.to_string())
-                .step_action(action.clone())
+                .node_action(action.clone())
                 .build()?;
-            self.dag.add_child(self.root, (), step);
+            self.dag.add_child(self.root, (), node);
         }
 
         Ok(true)
     }
 
-    /// get_step_execution_list returns the execution list with
+    /// get_node_execution_list returns the execution list with
     /// children for ordered execution
     ///
-    /// `start_from` is an optional step name to get the execution list
+    /// `start_from` is an optional node name to get the execution list
     /// from vs. the root
-    pub fn get_step_execution_list<'a>(
+    pub fn get_node_execution_list<'a>(
         &'a self,
         start_from: Option<String>,
-    ) -> AppResult<StepList> {
-        let mut step_list = StepList::new();
+    ) -> AppResult<NodeList> {
+        let mut node_list = NodeList::new();
 
-        let steps = if let Some(start_step) = start_from {
-            info!("Reduced run... starting from {}", &start_step);
-            self.get_nodes_in_order_from(&start_step)
+        let nodes = if let Some(start_node) = start_from {
+            info!("Reduced run... starting from {}", &start_node);
+            self.get_nodes_in_order_from(&start_node)
         } else {
             self.get_nodes_in_order()
         };
 
-        if let Err(e) = steps {
+        if let Err(e) = nodes {
             return Err(e);
         }
-        let steps = steps.unwrap();
+        let nodes = nodes.unwrap();
 
-        for lvl in steps.iter() {
-            let step_group = lvl
+        for lvl in nodes.iter() {
+            let node_group = lvl
                 .iter()
-                .map(|step| Node {
-                    name: step.name.clone(),
-                    state: step.state.clone(),
-                    package_data: step.package_data.clone(),
-                    trigger: step.trigger.clone(),
-                    step_action: step.step_action.clone(),
-                    depends_on: step.depends_on.clone(),
-                    run_started: step.run_started.clone(),
-                    input: step.input.clone(),
+                .map(|node| Node {
+                    name: node.name.clone(),
+                    state: node.state.clone(),
+                    package_data: node.package_data.clone(),
+                    trigger: node.trigger.clone(),
+                    node_action: node.node_action.clone(),
+                    depends_on: node.depends_on.clone(),
+                    run_started: node.run_started.clone(),
+                    input: node.input.clone(),
                 })
                 .collect::<Vec<Node>>();
-            match step_list.add_list(step_group) {
+            match node_list.add_list(node_group) {
                 Ok(_) => (),
                 Err(e) => return Err(e),
             }
         }
 
-        for lvl in steps.iter() {
-            for step in lvl.iter() {
-                for dep in step.depends_on.iter() {
-                    if step_list.is_step_name_parent(&dep)
-                        && step_list.is_step_name_parent(&step.name)
+        for lvl in nodes.iter() {
+            for node in lvl.iter() {
+                for dep in node.depends_on.iter() {
+                    if node_list.is_node_name_parent(&dep)
+                        && node_list.is_node_name_parent(&node.name)
                     {
-                        match step_list.set_child(&dep, &step.name) {
+                        match node_list.set_child(&dep, &node.name) {
                             Err(_e) => {
-                                return Err(AppError::FlowStepError(format!(
+                                return Err(AppError::FlowNodeError(format!(
                                     "Could not add '{}' to child '{}'",
-                                    dep, step.name
+                                    dep, node.name
                                 )))
                             }
                             Ok(_) => (),
@@ -216,11 +216,11 @@ impl Flow {
             }
         }
 
-        Ok(step_list)
+        Ok(node_list)
     }
 
     /// get_nodes_in_order
-    /// returns the next steps starting from the `start_index`
+    /// returns the next nodes starting from the `start_index`
     ///
     /// Example:
     ///     flow.get_nodes_in_order_from(parent);
@@ -228,17 +228,17 @@ impl Flow {
         &'a self,
         start_from: &str,
     ) -> AppResult<Vec<Vec<&'a Node>>> {
-        if let Some((idx, step)) = self.find_node_by_name(start_from) {
+        if let Some((idx, node)) = self.find_node_by_name(start_from) {
             match self.get_nodes_in_order_from_node_index(idx) {
                 Err(e) => Err(e),
-                Ok(mut steps) => {
-                    steps.insert(0, vec![step]);
-                    Ok(steps)
+                Ok(mut nodes) => {
+                    nodes.insert(0, vec![node]);
+                    Ok(nodes)
                 }
             }
         } else {
             Err(AppError::FlowError(
-                format!("Cannot start from {} - steps do not exist", start_from).to_string(),
+                format!("Cannot start from {} - nodes do not exist", start_from).to_string(),
             ))
         }
     }
@@ -318,7 +318,7 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_flow_can_add_a_single_simple_step_for_two_step_sequence() {
+    fn test_flow_can_add_a_single_simple_node_for_two_node_sequence() {
         let mut flow = Flow::new();
         let action = make_action(
             "find_one_action",
@@ -328,52 +328,52 @@ mod test {
                 args: Vec::default(),
             }),
         );
-        let mut step = make_node("first_step", &vec![]);
-        step.step_action = action;
-        let res = flow.add_step_obj(&step);
+        let mut node = make_node("first_node", &vec![]);
+        node.node_action = action;
+        let res = flow.add_node_obj(&node);
         assert!(res.is_ok());
 
-        let steps = flow.get_nodes_in_order().ok();
-        assert!(steps.is_some());
-        let steps = steps.unwrap();
-        assert_eq!(1, steps.len());
+        let nodes = flow.get_nodes_in_order().ok();
+        assert!(nodes.is_some());
+        let nodes = nodes.unwrap();
+        assert_eq!(1, nodes.len());
     }
 
     #[test]
-    fn test_flow_can_add_a_series_of_dependent_command_steps_for_two_step_sequence() {
-        let (flow, _orig_steps) = make_two_step_sequence_flow();
+    fn test_flow_can_add_a_series_of_dependent_command_nodes_for_two_node_sequence() {
+        let (flow, _orig_nodes) = make_two_node_sequence_flow();
 
-        let steps = flow.get_nodes_in_order().ok();
-        assert!(steps.is_some());
-        let steps = steps.unwrap();
-        assert_eq!(2, steps.len());
+        let nodes = flow.get_nodes_in_order().ok();
+        assert!(nodes.is_some());
+        let nodes = nodes.unwrap();
+        assert_eq!(2, nodes.len());
 
-        let first_steps = steps.get(0).unwrap();
-        let first_step = first_steps.get(0).unwrap();
-        assert_eq!(first_step.name, "get_weather_step");
+        let first_nodes = nodes.get(0).unwrap();
+        let first_node = first_nodes.get(0).unwrap();
+        assert_eq!(first_node.name, "get_weather_node");
 
-        let second_step = steps.get(1).unwrap().get(0).unwrap();
-        assert_eq!(second_step.name, "print_forecast");
+        let second_node = nodes.get(1).unwrap().get(0).unwrap();
+        assert_eq!(second_node.name, "print_forecast");
     }
 
     #[test]
-    fn test_get_execution_list_in_order_for_two_step_sequence() {
-        let (flow, _orig_steps) = make_two_step_sequence_flow();
-        let execution_list = flow.get_step_execution_list(None);
+    fn test_get_execution_list_in_order_for_two_node_sequence() {
+        let (flow, _orig_nodes) = make_two_node_sequence_flow();
+        let execution_list = flow.get_node_execution_list(None);
         assert!(execution_list.is_ok());
         let sl = execution_list.unwrap();
-        assert_eq!(sl.steps.len(), 2);
+        assert_eq!(sl.nodes.len(), 2);
 
         // let mut sl2 = sl.clone();
-        let level_one = sl.steps.get(0).unwrap();
-        let level_two = sl.steps.get(1).unwrap();
+        let level_one = sl.nodes.get(0).unwrap();
+        let level_two = sl.nodes.get(1).unwrap();
         assert_eq!(1, level_one.len());
         assert_eq!(1, level_two.len());
     }
 
     #[test]
     fn test_flow_as_graphviz_dot_file() {
-        let (mut flow, _orig_steps) = make_two_step_sequence_flow();
+        let (mut flow, _orig_nodes) = make_two_node_sequence_flow();
         let action = make_action(
             "print-to-screen",
             ActionType::Shell(ShellAction {
@@ -382,9 +382,9 @@ mod test {
                 args: Vec::default(),
             }),
         );
-        let mut step = make_node("output-forecast", &vec!["print_forecast"]);
-        step.step_action = action;
-        add_step_obj(&mut flow, &step).ok();
+        let mut node = make_node("output-forecast", &vec!["print_forecast"]);
+        node.node_action = action;
+        add_node_obj(&mut flow, &node).ok();
 
         let graphviz = flow.as_dotfile(None);
         assert!(graphviz.len() > 0);
