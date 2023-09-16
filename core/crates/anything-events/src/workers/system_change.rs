2e360dbe461760f5f813e1ba1514c827a419dbee
Ari <me@ari.io>
2023-09-15 19:27:54 -0700

    Cleaned up leftover code in the system change worker

--- core/crates/anything-events/src/workers/system_change.rs
+++ core/crates/anything-events/src/workers/system_change.rs
@@ -15,20 +15,7 @@ use crate::{
 // For now, just watches the flow directory change
 pub async fn file_watcher(server: Arc<Server>) -> anyhow::Result<()> {
     let mut directory_change_tx = server.post_office.post_mail::<ChangeMessage>().await?;
-    // let mut execute_tx = server.post_office.post_mail::<ChangeMessage>().await?;
-    // let mut token_tx = server.post_office.post_mail::<ChangeMessage>().await?;
-    // let (mut tx, rx) = std::sync::mpsc::Sender<Result<notify::Event, notify::Error>>
 
-    // let (mut tx, mut rx) = postage::mpsc::channel::<notify::Event>(128);
-    // let (mut tx, rx) = postage::mpsc::channel::<notify::Event>(128);
-    // let (tx, rx) = tokio::sync::broadcast::channel::<notify::Event>(128);
-    // // let (tx, rx) = std::sync::mpsc::channel();
-    // let mut watcher = RecommendedWatcher::new(
-    //     move |res| {
-    //         futures::executor::block_on(tx.send(res)).unwrap();
-    //     },
-    //     Config::default(),
-    // )?;
     let config = server.context.config.clone();
     // watcher.watch(config.root_dir.as_path(), RecursiveMode::Recursive)?;
     let (mut watcher, mut rx) = async_watcher()?;
@@ -47,32 +34,13 @@ pub async fn file_watcher(server: Arc<Server>) -> anyhow::Result<()> {
         }
     }
 
-    // while let Ok(change_evt) = rx.recv() {
-    //     match change_evt {
-    //         Ok(event) => {
-    //             info!("event: {:?}", event);
-    //             directory_change_tx
-    //                 .send(ChangeMessage {
-    //                     kind: event.kind.into(),
-    //                     paths: event.paths,
-    //                 })
-    //                 .await?;
-    //         }
-    //         Err(e) => info!("watch error: {:?}", e),
-    //     }
-    // }
-
     Ok(())
 }
 
 // A worker to handle any changes in flow
 pub async fn handle_system_change(server: Arc<Server>) -> anyhow::Result<()> {
-    // let directory_change_rx = server.post_office.receive_mail::<ChangeMessage>().await?;
     let mut directory_change_rx = server.post_office.receive_mail::<ChangeMessage>().await?;
 
-    // let flow_handler: FlowHandler = FlowHandler::new();
-    // let current_flows: HashSet<Flow> = HashSet::new();
-    // println!("msg => {:?}", msg);
     while let Some(msg) = directory_change_rx.recv().await {
         // while let msg = directory_change_rx.recv().await {
         match msg.change_type {
@@ -91,8 +59,6 @@ fn async_watcher() -> notify::Result<(RecommendedWatcher, Receiver<notify::Resul
 {
     let (mut tx, rx) = channel(1);
 
-    // Automatically select the best implementation for your platform.
-    // You can also access each implementation directly e.g. INotifyWatcher.
     let watcher = RecommendedWatcher::new(
         move |res| {
             futures::executor::block_on(async {
