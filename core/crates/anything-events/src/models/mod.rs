fc26465167abcfd232a586512a49a0448216ada1
Ari <me@ari.io>
2023-09-19 18:01:10 -0700

    Added trigger repo

--- core/crates/anything-events/src/models/mod.rs
+++ core/crates/anything-events/src/models/mod.rs
@@ -3,3 +3,4 @@ pub(crate) mod flow;
 pub(crate) mod serialization;
 pub(crate) mod system_handler;
 pub(crate) mod tag;
+pub(crate) mod trigger;
--- core/crates/anything-events/src/repositories/trigger_repo.rs
+++ core/crates/anything-events/src/repositories/trigger_repo.rs
@@ -1,4 +1,10 @@
+use chrono::Utc;
 use sqlx::SqlitePool;
+
+use crate::{
+    errors::{EventsError, EventsResult},
+    models::trigger::{CreateTrigger, Trigger, TriggerId},
+};
 
 #[derive(Debug, Clone)]
 pub struct TriggerRepoImpl {
@@ -14,9 +20,33 @@ impl TriggerRepoImpl {
 
 #[async_trait::async_trait]
 pub trait FlowRepo {
+    async fn create_trigger(&self, create_trigger: CreateTrigger) -> EventsResult<TriggerId>;
     // async fn get_flows(&self) -> EventsResult<Flow>;
     // async fn get_flow_by_id(&self, event_id: EventId) -> EventsResult<Event>;
 }
 
 #[async_trait::async_trait]
-impl FlowRepo for TriggerRepoImpl {}
+impl FlowRepo for TriggerRepoImpl {
+    async fn create_trigger(&self, create_trigger: CreateTrigger) -> EventsResult<TriggerId> {
+        let row = sqlx::query(
+            r#"
+        INSERT INTO triggers (trigger_id, event_name, payload, metadata, timestamp)
+        VALUES (?1, ?2, ?3, ?4, ?5)
+        RETURNING trigger_id
+        "#,
+        )
+        .bind(uuid::Uuid::new_v4().to_string())
+        .bind(create_trigger.event_name)
+        .bind(create_trigger.payload)
+        .bind(create_trigger.metadata)
+        .bind(Utc::now())
+        .fetch_one(&self.pool)
+        .await
+        .map_err(|e| {
+            EventsError::DatabaseError(crate::errors::DatabaseError::DBError(Box::new(e)))
+        })?;
+
+        let id = row.get("trigger_id");
+        Ok(id)
+    }
+}
