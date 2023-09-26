580930f435f5f8b2a7a48ded9e2a2ebeb77b6f80
Carl Lippert <carl.lippert@gmail.com>
2023-09-26 11:00:48 -0600

    search component

---
+++ tauri/src/components/baseSearch.tsx
@@ -0,0 +1,47 @@
+import React, { ChangeEvent, MouseEventHandler } from "react";
+
+type BaseSearchProps = {
+  value: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  onClick: MouseEventHandler<HTMLButtonElement>;
+  placeholder?: string;
+};
+
+const BaseSearch: React.FC<BaseSearchProps> = ({
+  value,
+  onChange,
+  onClick,
+  placeholder = "Search…",
+}) => {
+  return (
+    <div className="form-control w-full">
+      <div className="input-group w-full max-w-96">
+        <input
+          type="text"
+          placeholder={placeholder}
+          value={value}
+          className="input input-bordered w-full"
+          onChange={onChange}
+        />
+        <button className="btn btn-square" onClick={onClick}>
+          <svg
+            xmlns="http:www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default BaseSearch;
