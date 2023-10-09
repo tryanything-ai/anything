52f31f35e0669f7a7ded04b76b9b504b8cf7ce08
Carl Lippert <carl.lippert@gmail.com>
2023-10-09 18:11:51 -0300

    template page and templates looking much closer to the goal

--- web/components/templateCard.tsx
+++ web/components/templateCard.tsx
@@ -1,9 +1,8 @@
 import Link from "next/link";
 import { Database } from "@/types/supabase.types";
 import BaseNodeIcon from "./baseNodeIcons";
-import BaseCard from "./baseCard";
-// import Image from "next/image";
 import { Flow as LocalFlow } from "../../tauri/src/utils/newNodes";
+import clsx from "clsx";
 
 type Flow = Database["public"]["Tables"]["flow_templates"]["Row"];
 
@@ -14,8 +13,9 @@ export async function TemplateCard({ template }: { template: Flow }) {
       : template.flow_json;
 
   return (
-    <BaseCard
-      as={Link}
+    <Link
+      // className="cfard bg-base-300 shadow-xl my-2"
+      // as={Link}
       // image={
       //   <figure>
       //     <Image
@@ -25,30 +25,42 @@ export async function TemplateCard({ template }: { template: Flow }) {
       //   </figure>
       // }
       href={template.slug}
-      className="m-2 w-72 transition-all duration-200 ease-in-out transform hover:scale-105"
     >
-      <div className="card-body">
-        <h2 className="card-title">
-          {template.flow_name}
-          {/* <div className="badge badge-secondary">NEW</div> */}
-        </h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
+      <div className="card card-compact bg-base-300 overflow-hidden shadow-xl w-96 transition-all duration-200 ease-in-out transform hover:scale-105">
+        <div className="card-body">
+          <h2 className="card-title text-2xl text-ellipsis">
+            {template.flow_name}
+            {/* <div className="badge badge-secondary">NEW</div> */}
+          </h2>
+          <p>{flowJson.description}</p>
+          {/* //TODO: add tags from supabase. make them clickable */}
+          {/* <div className="card-actions justify-end">
           <div className="badge badge-outline">Fashion</div>
           <div className="badge badge-outline">Products</div>
+        </div> */}
+          {flowJson?.trigger?.icon && <NodeArray flow={flowJson} />}
         </div>
       </div>
-      {flowJson?.trigger?.icon && <NodeArray flow={flowJson} />}
-
-      <div className="p-6">
-        {/* <h2 className="text-lg font-semibold text-gray-700">{template.f}</h2> */}
-        {/* <p className="text-gray-500">{template}</p> */}
-      </div>
-    </BaseCard>
+    </Link>
   );
 }
 
 export const NodeArray = ({ flow }: { flow: LocalFlow }) => {
   //Loop through trigger and all actions to create icons
-  return <BaseNodeIcon icon={flow.trigger.icon} />;
+  const icons = [
+    flow.trigger.icon,
+    ...flow.actions.map((action) => action.icon),
+  ];
+  const visibleIcons = icons.slice(0, 4);
+  const hiddenIconsCount = icons.length - visibleIcons.length;
+
+  return (
+    <div className="flex flex-row gap-2">
+      {visibleIcons.map((icon, index) => (
+        <BaseNodeIcon key={index} icon={icon} className={clsx({ 'text-pink-500': index === 0 })} />
+      ))}
+      {hiddenIconsCount > 0 && <span>+{hiddenIconsCount}</span>}
+    </div>
+  );
+  // return <BaseNodeIcon icon={flow.trigger.icon} />;
 };
