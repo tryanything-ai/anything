e731d91cc0109d6772c6e16f6b0d5d6290dc6ec7
Carl Lippert <carl.lippert@gmail.com>
2023-10-09 17:39:08 -0300

    added sentry bug reporting to nextjs

---
+++ web/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://039dc60dfff0ff9bad479898055d1d7f@o4505953297498112.ingest.sentry.io/4505953410940928",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
