261b87273c88a615251446c971462abe0eba163d
Carl Lippert <carl.lippert@gmail.com>
2023-10-16 12:19:55 -0300

    env for sentry prepping for branch deploys on vercel to test og images

--- web/env.mjs
+++ web/env.mjs
@@ -3,6 +3,7 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
+    SENTRY_DSN: z.string().min(1),
     // SUPABASE_PROJECT_ID: process.env.SUPABASE_PROJECT_ID,
     // NEXTAUTH_URL: z.string().url().optional(),
     // NEXTAUTH_SECRET: z.string().min(1),
@@ -22,6 +23,7 @@ export const env = createEnv({
     NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string().min(1),
     NEXT_PUBLIC_POSTHOG_KEY: z.string().min(1),
     NEXT_PUBLIC_POSTHOG_HOST: z.string().min(1),
+    SENTRY_DSN: z.string().min(1),
   },
   runtimeEnv: {
     NEXT_PUBLIC_MOCK_ALL: process.env.NEXT_PUBLIC_MOCK_ALL,
@@ -30,6 +32,7 @@ export const env = createEnv({
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
     NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    SENTRY_DSN: process.env.SENTRY_DSN,
     // NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     // NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     // GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
