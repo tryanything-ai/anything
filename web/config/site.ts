c9de967d7c293bb945429568590675e70558798c
Carl Lippert <carl.lippert@gmail.com>
2023-10-17 12:29:18 -0300

    change the app url config to work with vercel.

--- web/config/site.ts
+++ web/config/site.ts
@@ -5,8 +5,8 @@ export const siteConfig: SiteConfig = {
   name: "Anything",
   description:
     "The Easiest way to automate your business with AI",
-  url: env.NEXT_PUBLIC_APP_URL,
-  ogImage: `${env.NEXT_PUBLIC_APP_URL}/og.jpg`,
+  url: env.NEXT_PUBLIC_VERCEL_URL,
+  ogImage: `${env.NEXT_PUBLIC_VERCEL_URL}/og.jpg`,
   links: {
     twitter: "https://twitter.com/carllippert",
     github: "https://github.com/tryanything-ai/anything",
