99f77559d2190884545a189d53f1c937a4296ec8
Carl Lippert <carl.lippert@gmail.com>
2023-10-17 16:22:19 -0300

    cleaning up how we do sentry.

--- web/sentry.edge.config.ts
+++ web/sentry.edge.config.ts
@@ -7,7 +7,7 @@ import * as Sentry from "@sentry/nextjs";
 import { env } from "@/env.mjs";
 if (process.env.NODE_ENV === "production") {
   Sentry.init({
-    dsn: env.SENTRY_DSN,
+    dsn: env.NEXT_PUBLIC_SENTRY_DSN,
     // Adjust this value in production, or use tracesSampler for greater control
     tracesSampleRate: 1,
 
