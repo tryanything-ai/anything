a07b7fd29ddfe725c7871ae5904a0b5059dd3bc1
Carl Lippert <carl.lippert@gmail.com>
2023-10-17 12:29:18 -0300

    change the app url config to work with vercel.

--- web/env.mjs
+++ web/env.mjs
@@ -16,7 +16,7 @@ export const env = createEnv({
     // POSTGRES_URL_NON_POOLING: z.string().min(1),
   },
   client: {
-    NEXT_PUBLIC_APP_URL: z.string().min(1),
+    NEXT_PUBLIC_VERCEL_URL: z.string().min(1),
     // SUPABASE_PROJECT_ID: z.string().min(1),
     NEXT_PUBLIC_SUPABASE_URL: z.string().min(1),
     NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string().min(1),
@@ -25,7 +25,7 @@ export const env = createEnv({
     SENTRY_DSN: z.string().min(1),
   },
   runtimeEnv: {
-    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NEXT_PUBLIC_VERCEL_URL: process.env.NEXT_PUBLIC_VERCEL_URL,
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
