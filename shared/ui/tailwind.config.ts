63afcb771dc4c4653f7df496397027b6332ef154
Carl Lippert <carl.lippert@gmail.com>
2023-10-23 12:49:51 -0300

    cleaning and trying to get ui to work as expected

--- shared/ui/tailwind.config.ts
+++ shared/ui/tailwind.config.ts
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    // content: ["./src/**/*.{html,js}"],
-    content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  // content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {},
+  },
+  corePlugins: { //https://miyauchi.dev/posts/lib-vite-tailwindcss/
+    preflight: false,
+  },
+  // prefix: 'ui-', //https://miyauchi.dev/posts/lib-vite-tailwindcss/
+  plugins: [], // 
+};
--- shared/ui/vite.config.ts
+++ shared/ui/vite.config.ts
@@ -1,25 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path';
-import dts from 'vite-plugin-dts';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import dts from "vite-plugin-dts";
+import { peerDependencies, dependencies } from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     emptyOutDir: false,
     lib: {
-      entry: path.resolve(__dirname, 'src/main.ts'),
-      formats: ['es'],
-      name: 'ui',
+      entry: path.resolve(__dirname, "src/main.ts"),
+      formats: ["es"],
+      name: "ui",
     },
     rollupOptions: {
-      external: ['react', 'react/jsx-runtime'],
+      // external: ['react', 'react/jsx-runtime'],
+      // rollupOptions: {
+      external: [
+        ...Object.keys(peerDependencies),
+        ...Object.keys(dependencies),
+      ],
+      // },
       // output: {
       //   globals: {
       //     React: 'react',
       //   },
       // },
     },
+    target: "esnext",
+    sourcemap: true,
   },
+
   plugins: [react(), dts()],
-})
+});
