1cde5bfdced6ea22779eeab810cb6408b6e705bc
Carl Lippert <carl.lippert@gmail.com>
2023-10-27 13:50:59 -0300

    consistent fonts across application

--- packages/tailwind-config/tailwind.config.ts
+++ packages/tailwind-config/tailwind.config.ts
@@ -5,22 +5,22 @@ const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}", "./index.html"],
   // https://github.com/vercel/turbo/tree/main/examples/with-tailwind
   theme: {
-    // fontSize: {f
-    //   "2xs": ["0.75rem", { lineHeight: "1.25rem" }],
-    //   xs: ["0.8125rem", { lineHeight: "1.5rem" }],
-    //   sm: ["0.875rem", { lineHeight: "1.5rem" }],
-    //   base: ["1rem", { lineHeight: "1.75rem" }],
-    //   lg: ["1.125rem", { lineHeight: "1.75rem" }],
-    //   xl: ["1.25rem", { lineHeight: "1.75rem" }],
-    //   "2xl": ["1.5rem", { lineHeight: "2rem" }],
-    //   "3xl": ["1.875rem", { lineHeight: "2.25rem" }],
-    //   "4xl": ["2.25rem", { lineHeight: "2.5rem" }],
-    //   "5xl": ["3rem", { lineHeight: "1" }],
-    //   "6xl": ["3.75rem", { lineHeight: "1" }],
-    //   "7xl": ["4.5rem", { lineHeight: "1" }],
-    //   "8xl": ["6rem", { lineHeight: "1" }],
-    //   "9xl": ["8rem", { lineHeight: "1" }],
-    // },
+    fontSize: {
+      "2xs": ["0.75rem", { lineHeight: "1.25rem" }],
+      xs: ["0.8125rem", { lineHeight: "1.5rem" }],
+      sm: ["0.875rem", { lineHeight: "1.5rem" }],
+      base: ["1rem", { lineHeight: "1.75rem" }],
+      lg: ["1.125rem", { lineHeight: "1.75rem" }],
+      xl: ["1.25rem", { lineHeight: "1.75rem" }],
+      "2xl": ["1.5rem", { lineHeight: "2rem" }],
+      "3xl": ["1.875rem", { lineHeight: "2.25rem" }],
+      "4xl": ["2.25rem", { lineHeight: "2.5rem" }],
+      "5xl": ["3rem", { lineHeight: "1" }],
+      "6xl": ["3.75rem", { lineHeight: "1" }],
+      "7xl": ["4.5rem", { lineHeight: "1" }],
+      "8xl": ["6rem", { lineHeight: "1" }],
+      "9xl": ["8rem", { lineHeight: "1" }],
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
@@ -32,7 +32,7 @@ const config: Config = {
         //     },
       },
       fontFamily: {
-        // sans: ["var(--font-inter)", ...defaultTheme.fontFamily.sans],
+        sans: ["var(--font-inter)", ...defaultTheme.fontFamily.sans],
         display: ["var(--font-dm-sans)", ...defaultTheme.fontFamily.sans],
       },
       colors: {
@@ -68,7 +68,7 @@ const config: Config = {
     },
   },
   plugins: [
-    require("@tailwindcss/typography"),
+    // require("@tailwindcss/typography"),
     require("tailwindcss-animate"),
     require("@tailwindcss/forms"),
     require("daisyui"),
