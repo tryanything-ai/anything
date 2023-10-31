3058e399a7d372c14db5f8b020d2f8c6bd2cae10
Carl Lippert <carl.lippert@gmail.com>
2023-10-31 11:53:57 -0300

    goofy 404

--- apps/tauri/src/error-page.tsx
+++ apps/tauri/src/error-page.tsx
@@ -1,22 +1,24 @@
-import { Link,useRouteError  } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
+import PageLayout from "./pageLayout";
 
 export default function ErrorPage() {
   const error: any = useRouteError();
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred. - Carl</p>
+    <PageLayout>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <img src="/404.svg" alt="Error 404" style={{ maxWidth: '100%', maxHeight: '100%' }} />
+      </div>
       <p>
         Go{" "}
-        <Link className="underline" to="/">
+        <Link className="btn btn-primary" to="/">
           home
         </Link>
       </p>
       <p>
         <i>{error.statusText || error.message}</i>
       </p>
-    </div>
+    </PageLayout>
   );
 }
