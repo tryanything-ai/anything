a3abe4d4a34cc0ec40f3e711f996fef949d2362a
Carl Lippert <carl.lippert@gmail.com>
2023-11-08 17:42:23 -0300

    dark mode everywhere

--- apps/tauri/src/layout.tsx
+++ apps/tauri/src/layout.tsx
@@ -13,7 +13,10 @@ import AllContext from "./context/context";
 export default function Layout() {
   return (
     <AllContext>
-      <div className="flex flex-row w-screen h-screen text-slate-12 font-sans">
+      <div
+        data-theme="dark"
+        className="flex flex-row w-screen h-screen text-slate-12 font-sans"
+      >
         <div className="w-14 flex flex-col gap-3 pt-3 pb-2 border-r border-slate-6 flex-shrink-0">
           <NavLink link="/" icon={VscHome} />
           <NavLink link="/templates" icon={VscGlobe} />
