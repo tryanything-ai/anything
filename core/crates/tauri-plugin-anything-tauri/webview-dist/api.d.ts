d965eacb687b7ebd0ee53378e167b03fb66c1761
Carl Lippert <carl.lippert@gmail.com>
2024-04-26 19:55:38 -0300

    cleaning out old plugin implementation

--- core/crates/tauri-plugin-anything-tauri/webview-dist/api.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class Anything {
-    path: string;
-    constructor(path: string);
-    stop(): Promise<unknown>;
-    getFlows<T>(): Promise<T>;
-    createFlow<T>(flowName: string, flowId: string): Promise<T>;
-}
