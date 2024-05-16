e85a90de402662aa057af44a9ef114cbd79e0844
Carl Lippert <carl.lippert@gmail.com>
2024-05-16 16:52:07 -0300

    register function returning json

--- plugin-core/crates/pdk-test-runner/src/lib.rs
+++ plugin-core/crates/pdk-test-runner/src/lib.rs
@@ -1,5 +1,14 @@
 use extism_pdk::*;
+use serde::{Deserialize, Serialize};
 use xtp_test;
+
+#[derive(Serialize, Deserialize)]
+struct Action {
+    pub id: String,
+    pub name: String,
+    pub description: String,
+    pub timestamp: String,
+}
 
 // You _must_ export a single `test` function for the runner to execute.
 #[plugin_fn]
@@ -10,6 +19,10 @@ pub fn test() -> FnResult<()> {
     // assert the count of the vowels is correct, giving the test case a name (which will be shown in the CLI output)
     // using the macro version here will also capture filename and line number
     xtp_test::assert_eq!("response is as expected", res, "carls test input");
+
+    let Json(action): Json<Action> = xtp_test::call("register", "")?;
+
+    xtp_test::assert_eq!("action id is 1", action.id, "1");
 
     Ok(())
 }
