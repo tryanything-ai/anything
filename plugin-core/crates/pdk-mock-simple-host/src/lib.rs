837f9724c87d21bb614a4cd8beb81afde5a93880
Carl Lippert <carl.lippert@gmail.com>
2024-05-17 13:57:34 -0300

    wired up testing for real plugins too.

---
+++ plugin-core/crates/pdk-mock-simple-host/src/lib.rs
@@ -0,0 +1,4 @@
+// create a main 
+// have it load plugin from file
+// have it call register on them 
+// have it call execute on them
--- plugin-core/crates/plugins/anything-http-plugin/src/lib.rs
+++ plugin-core/crates/plugins/anything-http-plugin/src/lib.rs
@@ -1,14 +1,34 @@
-use anything_pdk::*;
 use extism_pdk::*;
 use serde::Deserialize;
 
-#[host_fn]
-extern "ExtismHost" {
-    fn host_log(input: Json<HostLogInput>);
+#[derive(serde::Serialize, ToBytes)]
+#[encoding(Json)]
+struct Action {
+    pub id: String,
+    pub name: String,
+    pub description: String,
+    pub timestamp: String,
 }
 
 #[plugin_fn]
-pub fn execute(Json(req): Json<HttpRequest>) -> FnResult<Vec<u8>> {
-    let res = http::request::<()>(&req, None)?;
-    Ok(res.body())
+pub fn execute(message: String) -> FnResult<String> {
+    // let _res = unsafe { host_log(message.clone())? };
+    Ok(message)
+}
+// #[plugin_fn]
+// pub fn execute(Json(req): Json<HttpRequest>) -> FnResult<Vec<u8>> {
+//     let res = http::request::<()>(&req, None)?;
+//     Ok(res.body())
+// }
+
+#[plugin_fn]
+pub fn register() -> FnResult<Action> {
+    let action = Action {
+        id: "1".to_string(),
+        name: "register".to_string(),
+        description: "register a new user".to_string(),
+        timestamp: "2021-09-01".to_string(),
+    };
+
+    Ok(action)
 }
