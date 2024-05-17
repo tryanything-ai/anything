3f131057cc83497d4b6d12fa1d2aaecb528570eb
Carl Lippert <carl.lippert@gmail.com>
2024-05-17 14:18:45 -0300

    restructuring common structs into anything_pdk

--- plugin-core/crates/pdk-mock-plugin/src/lib.rs
+++ plugin-core/crates/pdk-mock-plugin/src/lib.rs
@@ -1,28 +1,6 @@
+use anything_pdk::{Action, Log, Event};
 use extism_pdk::*;
 use serde::{Deserialize, Serialize};
-
-#[derive(Deserialize, Serialize)]
-struct Log {
-    pub time: String,
-    pub message: String,
-}
-
-#[derive(Deserialize, Serialize)]
-struct Event {
-    pub id: String,
-    pub name: String,
-    pub description: String,
-    pub timestamp: String,
-}
-
-#[derive(serde::Serialize, ToBytes)]
-#[encoding(Json)]
-struct Action {
-    pub id: String,
-    pub name: String,
-    pub description: String,
-    pub timestamp: String,
-}
 
 #[host_fn]
 extern "ExtismHost" {
@@ -35,7 +13,6 @@ pub fn execute(message: String) -> FnResult<String> {
     let _res = unsafe { host_log(message.clone())? };
     Ok(message)
 }
-
 
 #[plugin_fn]
 pub fn register() -> FnResult<Action> {
--- plugin-core/crates/pdk/src/lib.rs
+++ plugin-core/crates/pdk/src/lib.rs
@@ -1,27 +1,53 @@
-use extism::{CurrentPlugin, Error, Function, UserData, Val, ValType};
-use serde_json::Value;
+// use extism::{CurrentPlugin, Error, Function, Json, UserData, Val, ValType};
+// use serde_json::Value;
+
+use extism_pdk::*;
+use serde::{Deserialize, Serialize};
+//export a type used for register function
+#[derive(serde::Serialize, ToBytes)]
+#[encoding(Json)]
+pub struct Action {
+    pub id: String,
+    pub name: String,
+    pub description: String,
+    pub timestamp: String,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Log {
+    pub time: String,
+    pub message: String,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Event {
+    pub id: String,
+    pub name: String,
+    pub description: String,
+    pub timestamp: String,
+}
 
 //How Proto calls this https://github.com/moonrepo/proto/blob/d423b236948211aa644c4a8389f1f92343936260/crates/core/src/tool.rs
-pub fn create_host_functions() -> Vec<Function> {
-    vec![Function::new(
-        "host_log",
-        [ValType::I64],
-        [],
-        UserData::new(()),
-        host_log,
-    )]
-}
+// pub fn create_host_functions() -> Vec<Function> {
+//     vec![Function::new(
+//         "host_log",
+//         [ValType::I64],
+//         [],
+//         UserData::new(()),
+//         host_log,
+//     )]
+// }
 
-fn host_log(
-    plugin: &mut CurrentPlugin,
-    inputs: &[Val],
-    _outputs: &mut [Val],
-    _user_data: UserData<()>,
-) -> Result<(), Error> {
-    let input: Value = serde_json::from_str(plugin.memory_get_val(&inputs[0])?)?;
-    let message = input["message"].as_str().unwrap_or("No message provided");
+// fn host_log(
+//     plugin: &mut CurrentPlugin,
+//     inputs: &[Val],
+//     _outputs: &mut [Val],
+//     _user_data: UserData<()>,
+// ) -> Result<(), Error> {
+//     let input: Value = serde_json::from_str(plugin.memory_get_val(&inputs[0])?)?;
+//     let message = input["message"].as_str().unwrap_or("No message provided");
 
-    println!("Host Function Logged: {}", message);
+//     println!("Host Function Logged: {}", message);
 
-    Ok(())
-}
+//     Ok(())
+// }
