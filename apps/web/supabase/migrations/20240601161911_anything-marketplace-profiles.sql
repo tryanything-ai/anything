bf2d76ff6c2fe3309163dc5678b74fbf83fad17c
Carl Lippert <carl.lippert@gmail.com>
2024-06-01 14:42:49 -0300

    migrations for marketplace

---
+++ apps/web/supabase/migrations/20240601161911_anything-marketplace-profiles.sql
@@ -0,0 +1,128 @@
+
+-- Create Table For Profiles
+CREATE TABLE IF NOT EXISTS public.profiles
+(
+    id uuid unique NOT NULL DEFAULT uuid_generate_v4() primary key,
+    -- If your model is owned by an account, you want to make sure you have an account_id column
+    -- referencing the account table. Make sure you also set permissions appropriately
+    account_id uuid not null references accounts(id),
+
+    -- ADD YOUR COLUMNS HERE
+    id uuid not null,
+    updated_at timestamp with time zone null,
+    username text null,
+    full_name text null,
+    avatar_url text null default 'https://fokcbrnvhnwnwwpiqkdc.supabase.co/storage/v1/object/public/mocks/botttsNeutral-1698715092376.png'::text,
+    website text null,
+    twitter text null,
+    tiktok text null,
+    instagram text null,
+    youtube text null,
+    linkedin text null,
+    github text null,
+    public boolean not null default false,
+    bio text null,
+    constraint profiles_username_key unique (username),
+    constraint profiles_id_fkey foreign key (id) references auth.users(id) on delete cascade,
+    constraint username_length check ((char_length(username) >= 3))
+    -- timestamps are useful for auditing
+    -- Basejump has some convenience functions defined below for automatically handling these
+    updated_at timestamp with time zone,
+    created_at timestamp with time zone,
+    -- Useful for tracking who made changes to a record
+    -- Basejump has some convenience functions defined below for automatically handling these
+    updated_by uuid references auth.users(id),
+    created_by uuid references auth.users(id),
+);
+
+
+-- protect the timestamps by setting created_at and updated_at to be read-only and managed by a trigger
+CREATE TRIGGER set_profiles_timestamp
+    BEFORE INSERT OR UPDATE ON public.profiles
+    FOR EACH ROW
+EXECUTE PROCEDURE basejump.trigger_set_timestamps();
+
+-- protect the updated_by and created_by columns by setting them to be read-only and managed by a trigger
+CREATE TRIGGER set_profiles_user_tracking
+    BEFORE INSERT OR UPDATE ON public.profiles
+    FOR EACH ROW
+EXECUTE PROCEDURE basejump.trigger_set_user_tracking();
+
+
+-- enable RLS on the table
+ALTER TABLE public.profiles ENABLE ROW LEVEL SECURITY;
+
+
+-- Because RLS is enabled, this table will NOT be accessible to any users by default
+-- You must create a policy for each user that should have access to the table
+-- Here are a few example policies that you may find useful when working with Basejump
+
+----------------
+-- Authenticated users should be able to read all records regardless of account
+----------------
+create policy "All logged in users can select" on public.profiles
+    for select
+    to authenticated
+    using (true);
+
+----------------
+-- Authenticated AND Anon users should be able to read all records regardless of account
+----------------
+create policy "All authenticated and anonymous users can select" on public.profiles
+    for select
+    to authenticated, anon
+    using (true);
+
+-------------
+-- Users should be able to read records that are owned by an account they belong to
+--------------
+create policy "Account members can select" on public.profiles
+    for select
+    to authenticated
+    using (
+    (account_id IN ( SELECT basejump.get_accounts_with_role()))
+    );
+
+
+----------------
+-- Users should be able to create records that are owned by an account they belong to
+----------------
+create policy "Account members can insert" on public.profiles
+    for insert
+    to authenticated
+    with check (
+    (account_id IN ( SELECT basejump.get_accounts_with_role()))
+    );
+
+---------------
+-- Users should be able to update records that are owned by an account they belong to
+---------------
+create policy "Account members can update" on public.profiles
+    for update
+    to authenticated
+    using (
+    (account_id IN ( SELECT basejump.get_accounts_with_role()))
+    );
+
+----------------
+-- Users should be able to delete records that are owned by an account they belong to
+----------------
+-- create policy "Account members can delete" on public.profiles
+--     for delete
+--     to authenticated
+--     using (
+--     (account_id IN ( SELECT basejump.get_accounts_with_role()))
+--     );
+
+----------------
+-- Only account OWNERS should be able to delete records that are owned by an account they belong to
+----------------
+-- create policy "Account owners can delete" on public.profiles
+--     for delete
+--     to authenticated
+--     using (
+--     (account_id IN ( SELECT basejump.get_accounts_with_role("owner")))
+--      );
+
+
+
---
+++ apps/web/supabase/migrations/20240601162005_anything-marketplace-flow-template-tags.sql
@@ -0,0 +1,115 @@
+
+CREATE TABLE IF NOT EXISTS public.flow_template_tags
+(
+    tag_id uuid NOT NULL,
+    -- If your model is owned by an account, you want to make sure you have an account_id column
+    -- referencing the account table. Make sure you also set permissions appropriately
+    account_id uuid not null references accounts(id),
+
+    -- ADD YOUR COLUMNS HERE 
+    flow_template_id uuid not null,
+    constraint flow_tags_pkey primary key (tag_id, flow_template_id),
+    constraint flow_template_tags_flow_template_id_fkey foreign key (flow_template_id) references flow_templates (flow_template_id),
+    constraint flow_template_tags_tag_id_fkey foreign key (tag_id) references tags (id)
+    -- timestamps are useful for auditing
+    -- Basejump has some convenience functions defined below for automatically handling these
+    updated_at timestamp with time zone,
+    created_at timestamp with time zone,
+    -- Useful for tracking who made changes to a record
+    -- Basejump has some convenience functions defined below for automatically handling these
+    updated_by uuid references auth.users(id),
+    created_by uuid references auth.users(id),
+);
+
+
+-- protect the timestamps by setting created_at and updated_at to be read-only and managed by a trigger
+CREATE TRIGGER set_flow_template_tags_timestamp
+    BEFORE INSERT OR UPDATE ON public.flow_template_tags
+    FOR EACH ROW
+EXECUTE PROCEDURE basejump.trigger_set_timestamps();
+
+-- protect the updated_by and created_by columns by setting them to be read-only and managed by a trigger
+CREATE TRIGGER set_flow_template_tags_user_tracking
+    BEFORE INSERT OR UPDATE ON public.flow_template_tags
+    FOR EACH ROW
+EXECUTE PROCEDURE basejump.trigger_set_user_tracking();
+
+
+-- enable RLS on the table
+ALTER TABLE public.flow_template_tags ENABLE ROW LEVEL SECURITY;
+
+
+-- Because RLS is enabled, this table will NOT be accessible to any users by default
+-- You must create a policy for each user that should have access to the table
+-- Here are a few example policies that you may find useful when working with Basejump
+
+----------------
+-- Authenticated users should be able to read all records regardless of account
+----------------
+create policy "All logged in users can select" on public.flow_template_tags
+    for select
+    to authenticated
+    using (true);
+
+----------------
+-- Authenticated AND Anon users should be able to read all records regardless of account
+----------------
+create policy "All authenticated and anonymous users can select" on public.flow_template_tags
+    for select
+    to authenticated, anon
+    using (true);
+
+-------------
+-- Users should be able to read records that are owned by an account they belong to
+--------------
+-- create policy "Account members can select" on public.flow_template_tags
+--     for select
+--     to authenticated
+--     using (
+--     (account_id IN ( SELECT basejump.get_accounts_with_role()))
+--     );
+
+
+----------------
+-- Users should be able to create records that are owned by an account they belong to
+----------------
+-- create policy "Account members can insert" on public.flow_template_tags
+--     for insert
+--     to authenticated
+--     with check (
+--     (account_id IN ( SELECT basejump.get_accounts_with_role()))
+--     );
+
+---------------
+-- Users should be able to update records that are owned by an account they belong to
+---------------
+-- create policy "Account members can update" on public.flow_template_tags
+--     for update
+--     to authenticated
+--     using (
+--     (account_id IN ( SELECT basejump.get_accounts_with_role()))
+--     );
+
+----------------
+-- Users should be able to delete records that are owned by an account they belong to
+----------------
+-- create policy "Account members can delete" on public.flow_template_tags
+--     for delete
+--     to authenticated
+--     using (
+--     (account_id IN ( SELECT basejump.get_accounts_with_role()))
+--     );
+
+----------------
+-- Only account OWNERS should be able to delete records that are owned by an account they belong to
+----------------
+-- create policy "Account owners can delete" on public.flow_template_tags
+--     for delete
+--     to authenticated
+--     using (
+--     (account_id IN ( SELECT basejump.get_accounts_with_role("owner")))
+--      );
+
+
+
+
