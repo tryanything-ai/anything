4f9b018892faa48e9f2c24cd1aae4e8a57b1b5e9
Carl Lippert <carl.lippert@gmail.com>
2024-06-01 13:13:42 -0300

    boilerplate pages to start getting a structure going

--- apps/web/src/app/dashboard/(personalAccount)/layout.tsx
+++ apps/web/src/app/dashboard/(personalAccount)/layout.tsx
@@ -1,16 +1,24 @@
-import {createClient} from "@/lib/supabase/server";
+import { createClient } from "@/lib/supabase/server";
 import DashboardHeader from "@/components/dashboard/dashboard-header";
 
-export default async function PersonalAccountDashboard({children}: {children: React.ReactNode}) {
+export default async function PersonalAccountDashboard({ children }: { children: React.ReactNode }) {
 
     const supabaseClient = createClient();
 
-    const {data: personalAccount, error} = await supabaseClient.rpc('get_personal_account');
+    const { data: personalAccount, error } = await supabaseClient.rpc('get_personal_account');
 
     const navigation = [
         {
             name: 'Overview',
             href: '/dashboard',
+        },
+        {
+            name: 'Workflows',
+            href: '/dashboard/workflows'
+        },
+        {
+            name: "Templates",
+            href: '/dashboard/templates'
         },
         {
             name: 'Settings',
