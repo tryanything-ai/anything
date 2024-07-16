11131bee78cea9fc60482f8409d82dfacbfda717
Carl Lippert <carl.lippert@gmail.com>
2024-07-16 20:46:20 -0300

    boilerplate for tasks

--- apps/web/src/lib/anything-api/index.ts
+++ apps/web/src/lib/anything-api/index.ts
@@ -2,10 +2,12 @@ import * as flows from './flows';
 import * as action_templates from './action-templates';
 import * as secrets from './secrets';
 import * as testing from './testing';
+import * as tasks from './tasks'
 
 const api = {
   flows,
   testing,
+  tasks,
   action_templates,
   secrets
 };
