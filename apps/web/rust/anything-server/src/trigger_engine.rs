44500a7592e6fdc12d5d0943f4f70da83464983b
Carl Lippert <carl.lippert@gmail.com>
2024-07-19 12:59:26 -0300

    its alive. cron fired off a workflow.

--- apps/web/rust/anything-server/src/trigger_engine.rs
+++ apps/web/rust/anything-server/src/trigger_engine.rs
@@ -14,6 +14,9 @@ use tokio::sync::RwLock;
 use cron::Schedule;
 use serde_json::Value;
 use std::str::FromStr;
+use uuid::Uuid;
+
+use crate::workflow_types::{CreateTaskInput, TaskConfig};
 
 pub struct TriggerEngineState {
     pub triggers: Arc<RwLock<HashMap<String, InMemoryTrigger>>>,
@@ -21,6 +24,8 @@ pub struct TriggerEngineState {
 
 #[derive(Debug, Clone)]
 pub struct InMemoryTrigger {
+    pub account_id: String,                //account id
+    pub node_id: String,                   //node id
     pub trigger_id: String,                //type of trigger essentially
     pub flow_id: String,                   //flow id
     pub flow_version_id: String,           // Some identifier for the task related to the trigger
@@ -48,6 +53,7 @@ pub async fn cron_job_loop(state: Arc<AppState>) {
                 if should_trigger_run(trigger) {
                     // Execute the task associated with the trigger
                     println!("Running triggering task for trigger: {}", trigger.trigger_id);
+                    create_trigger_task(&state, trigger).await.unwrap();
                     // TODO: Execute the task by creating a trigger task
                     update_trigger_last_run(trigger, &trigger_state).await;
                 } else {
@@ -74,7 +80,7 @@ pub async fn hydrate_triggers(client: &Postgrest, triggers: &Arc<RwLock<HashMap<
     let response = match client
         .from("flow_versions")
         .auth(supabase_service_role_api_key.clone())
-        .select("flow_id, flow_version_id, flow_definition") // TODO: only fetch active flows
+        .select("flow_id, flow_version_id, flow_definition, account_id") // TODO: only fetch active flows
         .execute()
         .await
     {
@@ -106,16 +112,18 @@ pub async fn hydrate_triggers(client: &Postgrest, triggers: &Arc<RwLock<HashMap<
     let mut new_triggers = HashMap::new();
 
     for flow_version in flow_versions {
-        if let (Some(flow_id), Some(flow_version_id), Some(flow_definition)) = (
+        if let (Some(flow_id), Some(flow_version_id), Some(flow_definition), Some(account_id)) = (
             flow_version.get("flow_id").and_then(|v| v.as_str()),
             flow_version.get("flow_version_id").and_then(|v| v.as_str()),
             flow_version.get("flow_definition"),
+            flow_version.get("account_id").and_then(|v| v.as_str())
         ) {
             if let Some(actions) = flow_definition.get("actions").and_then(|v| v.as_array()) {
                 for action in actions {
-                    if let (Some(trigger_id), Some(trigger_type)) = (
-                        action.get("node_id").and_then(|v| v.as_str()),
+                    if let (Some(trigger_id), Some(trigger_type), Some(node_id)) = (
+                        action.get("plugin_id").and_then(|v| v.as_str()),
                         action.get("type").and_then(|v| v.as_str()),
+                        action.get("node_id").and_then(|v| v.as_str())
                     ) {
                         if trigger_type == "trigger" {
                             println!("Found trigger action of type trigger");
@@ -144,6 +152,8 @@ pub async fn hydrate_triggers(client: &Postgrest, triggers: &Arc<RwLock<HashMap<
                             };
 
                             let new_trigger = InMemoryTrigger {
+                                node_id: node_id.to_string(),
+                                account_id: account_id.to_string(),
                                 trigger_id: trigger_id.to_string(),
                                 flow_id: flow_id.to_string(),
                                 flow_version_id: flow_version_id.to_string(),
@@ -170,6 +180,8 @@ pub async fn hydrate_triggers(client: &Postgrest, triggers: &Arc<RwLock<HashMap<
                     }
                 }
             }
+        } else {
+            println!("Missing required fields in flow_version: {:?}", flow_version);
         }
     }
 
@@ -178,7 +190,6 @@ pub async fn hydrate_triggers(client: &Postgrest, triggers: &Arc<RwLock<HashMap<
         triggers.insert(id, trigger);
     }
 }
-
 
 pub fn should_trigger_run(trigger: &InMemoryTrigger) -> bool {
     let now = Utc::now();
@@ -223,94 +234,85 @@ async fn update_trigger_last_run(trigger: &InMemoryTrigger, triggers: &Arc<RwLoc
     triggers.insert(trigger.flow_version_id.clone(), updated_trigger);
 }
 
+
+async fn create_trigger_task(state: &AppState, trigger: &InMemoryTrigger) -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+    let supabase_service_role_api_key = env::var("SUPABASE_SERVICE_ROLE_API_KEY")?;
+    let client = &state.client;
+
+    println!("Handling create task from cron trigger");
+
+    let task_config = TaskConfig {
+        variables: trigger.config.get("variables").cloned().unwrap_or_default(),
+        inputs: trigger.config.get("input").cloned().unwrap_or_default(),
+    };
+
+    let input = CreateTaskInput {
+        account_id: trigger.account_id.clone(),
+        task_status: "pending".to_string(),
+        flow_id: trigger.flow_id.clone(),
+        flow_version_id: trigger.flow_version_id.clone(),
+        flow_version_name: "derp".to_string(),
+        trigger_id: trigger.trigger_id.clone(),
+        trigger_session_id: Uuid::new_v4().to_string(),
+        trigger_session_status: "pending".to_string(),
+        flow_session_id: Uuid::new_v4().to_string(),
+        flow_session_status: "pending".to_string(),
+        node_id: trigger.node_id.clone(),
+        is_trigger: true,
+        plugin_id: trigger.trigger_id.clone(),
+        stage: "production".to_string(),
+        config: serde_json::json!(task_config),
+        test_config: None,
+    };
+
+    let response = client
+        .from("tasks")
+        .auth(supabase_service_role_api_key)
+        .insert(serde_json::to_string(&input)?)
+        .execute()
+        .await?;
+
+    let body = response.text().await?;
+    let _items: Value = serde_json::from_str(&body)?;
+
+    if let Err(err) = state.task_signal.send(()) {
+        println!("Failed to send task signal: {:?}", err);
+    }
+
+    Ok(())
+}
 // async fn create_trigger_task()
-// Testing a workflow
-// async fn create_trigger_task(client: &Postgrest, trigger: &InMemoryTrigger) -> impl IntoResponse {
+// // Testing a workflow
+// async fn create_trigger_task(state: &AppState, trigger: &InMemoryTrigger) {
+
+//     dotenv().ok();
+//     let supabase_service_role_api_key = env::var("SUPABASE_SERVICE_ROLE_API_KEY").expect("SUPABASE_SERVICE_ROLE_API_KEY must be set");
 
 //     let client = &state.client;
 
-//     println!("Handling test workflow");
+//     println!("Handling create task from cron trigger");
 
-//     // GET the workflow_version
-//     let response = match client
-//         .from("flow_versions")
-//         // .auth(user.jwt.clone())
-//         .eq("flow_version_id", &workflow_version_id)
-//         .select("*")
-//         .execute()
-//         .await
-//     {
-//         Ok(response) => response,
-//         Err(_) => return (StatusCode::INTERNAL_SERVER_ERROR, "Failed to execute request").into_response(),
-//     };
-
-//     // println!("Response from flow_versions: {:?}", response);
-
-//     let body = match response.text().await {
-//         Ok(body) => body,
-//         Err(_) => return (StatusCode::INTERNAL_SERVER_ERROR, "Failed to read response body").into_response(),
-//     };
-
-//     // println!("Body from flow_versions: {:?}", body);
-
-//     let items: Value = match serde_json::from_str(&body) {
-//         Ok(items) => items,
-//         Err(_) => return (StatusCode::INTERNAL_SERVER_ERROR, "Failed to parse JSON").into_response(),
-//     };
-
-//     // println!("Items from flow_versions: {:?}", items);
-
-//     let db_version_def = match items.get(0) {
-//         Some(item) => item,
-//         None => return (StatusCode::INTERNAL_SERVER_ERROR, "Failed to get item zero").into_response(),
-//     };
-
-//     // println!("db_version_def: {:?}", db_version_def);
-
-//       // Parse response into Workflow type
-//     let flow_definition = match db_version_def.get("flow_definition") {
-//         Some(flow_definition) => flow_definition,
-//         None => return (StatusCode::INTERNAL_SERVER_ERROR, "Failed to get flow_definition").into_response(),
-//     };
-
-//     // println!("flow_definition: {:?}", flow_definition);
-
-//     let workflow: Workflow = match serde_json::from_value(flow_definition.clone()) {
-//         Ok(workflow) => workflow,
-//         Err(err) => {
-//             println!("Failed to parse flow_definition into Workflow: {:?}", err);
-//             return (StatusCode::INTERNAL_SERVER_ERROR, format!("Failed to parse flow_definition into Workflow: {:?}", err)).into_response();
-//         }
-//     };
-
-//     // Use the `workflow` variable as needed
-//     // println!("Workflow Definition {:#?}", workflow);
-
-//     //PARSE RESPONSE. 
-//     //db_version_def.flow_definition is the Workflow type
-
-//     //TODO: call the engine
-//     //OR just create a task with the correct type and data
 
 //     let taskConfig = TaskConfig {
-//         variables: serde_json::json!(workflow.actions[0].variables), 
-//         inputs: serde_json::json!(workflow.actions[0].input), 
+//         variables: serde_json::json!(trigger.config.get("variables").cloned().unwrap_or_default()), 
+//         inputs: serde_json::json!(trigger.config.get("input").cloned().unwrap_or_default()), 
 //     }; 
 
 //     let input = CreateTaskInput {
-//         account_id: user.account_id.clone(),
+//         account_id: trigger.account_id.clone(),
 //         task_status: "pending".to_string(),
-//         flow_id: workflow_id.clone(),
-//         flow_version_id: workflow_version_id.clone(),
+//         flow_id: trigger.flow_id.clone(),
+//         flow_version_id: trigger.flow_version_id.clone(),
 //         flow_version_name: "derp".to_string(),
-//         trigger_id: workflow.actions[0].node_id.clone(),
+//         trigger_id: trigger.trigger_id.clone(),
 //         trigger_session_id: Uuid::new_v4().to_string(),
 //         trigger_session_status: "pending".to_string(),
 //         flow_session_id: Uuid::new_v4().to_string(),
 //         flow_session_status: "pending".to_string(),
-//         node_id: workflow.actions[0].node_id.clone(),
+//         node_id: trigger.node_id.clone(),
 //         is_trigger: true,
-//         plugin_id: workflow.actions[0].plugin_id.clone(),
+//         plugin_id: trigger.trigger_id.clone(),
 //         stage: "production".to_string(),
 //         config: serde_json::json!(taskConfig), 
 //         test_config: None
@@ -348,5 +350,4 @@ async fn update_trigger_last_run(trigger: &InMemoryTrigger, triggers: &Arc<RwLoc
 //         println!("Failed to send task signal: {:?}", err);
 //     }
 
-//     Json(items).into_response()
 // }
