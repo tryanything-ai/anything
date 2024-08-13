53f00ca51be7debdca04ba51f3960e9be394d6e4
Carl Lippert <carl.lippert@gmail.com>
2024-08-13 16:04:11 -0300

    cloud accoutn selector

--- apps/web/src/components/dashboard/navigation-account-selector.tsx
+++ apps/web/src/components/dashboard/navigation-account-selector.tsx
@@ -1,19 +1,18 @@
-"use client";
+'use client'
 
-import { useRouter } from "next/navigation";
-// import AccountSelector from "@/components/basejump/account-selector";
+import {useRouter} from "next/navigation";
+import AccountSelector from "@/components/basejump/account-selector";
 
 interface Props {
-  accountId: string;
+    accountId: string;
 }
-export default function NavigatingAccountSelector({ accountId }: Props) {
-  const router = useRouter();
+export default function NavigatingAccountSelector({accountId}: Props) {
+    const router = useRouter();
 
-  return (
-    <> </>
-    // <AccountSelector
-    //     accountId={accountId}
-    //     onAccountSelected={(account) => router.push(account?.personal_account ? `/dashboard` : `/dashboard/${account?.slug}`)}
-    // />
-  );
+    return (
+        <AccountSelector
+            accountId={accountId}
+            onAccountSelected={(account) => router.push(account?.personal_account ? `/dashboard` : `/dashboard/${account?.slug}`)}
+        />
+    )
 }
