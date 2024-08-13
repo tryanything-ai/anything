58e0abe3945a34314c835e02103657b83f2d03ef
Carl Lippert <carl.lippert@gmail.com>
2024-08-13 15:23:58 -0300

    drop account selector to see how deep this problem is

--- apps/web/src/components/dashboard/navigation-account-selector.tsx
+++ apps/web/src/components/dashboard/navigation-account-selector.tsx
@@ -1,18 +1,19 @@
-'use client'
+"use client";
 
-import {useRouter} from "next/navigation";
+import { useRouter } from "next/navigation";
 import AccountSelector from "@/components/basejump/account-selector";
 
 interface Props {
-    accountId: string;
+  accountId: string;
 }
-export default function NavigatingAccountSelector({accountId}: Props) {
-    const router = useRouter();
+export default function NavigatingAccountSelector({ accountId }: Props) {
+  const router = useRouter();
 
-    return (
-        <AccountSelector
-            accountId={accountId}
-            onAccountSelected={(account) => router.push(account?.personal_account ? `/dashboard` : `/dashboard/${account?.slug}`)}
-        />
-    )
+  return (
+    <> </>
+    // <AccountSelector
+    //     accountId={accountId}
+    //     onAccountSelected={(account) => router.push(account?.personal_account ? `/dashboard` : `/dashboard/${account?.slug}`)}
+    // />
+  );
 }
