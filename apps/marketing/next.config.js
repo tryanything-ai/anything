f3bfc0d2c1d7f058c546d18335585cccb1bb8368
Carl Lippert <carl.lippert@gmail.com>
2024-08-14 19:28:45 -0300

    remove tailwind-config package

--- apps/marketing/next.config.js
+++ apps/marketing/next.config.js
@@ -1,10 +1,8 @@
 module.exports = {
-  // reactStrictMode: true,
   images: {
     loader: "custom",
     loaderFile: "./src/lib/supabaseImageLoader.ts",
   },
   transpilePackages: ["@repo/ui"],
-  // transpilePackages: ["utils", "ui"], //recommended here
   //https://turbo.build/repo/docs/handbook/sharing-code/internal-packages
 };
