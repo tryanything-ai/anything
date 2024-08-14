53f1fc63ca9d684b5af2835ea137f63160351717
Carl Lippert <carl.lippert@gmail.com>
2024-08-14 12:17:33 -0300

    standardizing more config

--- apps/marketing/next.config.js
+++ apps/marketing/next.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  reactStrictMode: true,
+  // reactStrictMode: true,
   images: {
     loader: "custom",
     loaderFile: "./src/lib/supabaseImageLoader.ts",
   },
+  transpilePackages: ["@repo/ui"],
   // transpilePackages: ["utils", "ui"], //recommended here
   //https://turbo.build/repo/docs/handbook/sharing-code/internal-packages
 };
--- apps/marketing/postcss.config.js
+++ apps/marketing/postcss.config.js
@@ -1,9 +1,1 @@
-// If you want to use other PostCSS plugins, see the following:
-// https://tailwindcss.com/docs/using-with-preprocessors
-
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+export { default } from "@repo/ui/postcss.config";
