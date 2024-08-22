c966a1b8fc67e625d859daeaf94156cb36b126ce
Carl Lippert <carl.lippert@gmail.com>
2024-08-21 19:50:13 -0500

    properly handle search params in callback

--- apps/web/src/app/auth/[provider]/callback/page.tsx
+++ apps/web/src/app/auth/[provider]/callback/page.tsx
@@ -1,14 +1,20 @@
 "use client";
 
 import { useEffect } from "react";
-import { useParams, useRouter } from "next/navigation";
+import { useParams, useRouter, useSearchParams } from "next/navigation";
 import api from "@/lib/anything-api"; // Adjust this import according to your API setup
 
 const OAuthCallbackPage = () => {
   const router = useRouter();
-  const { code, state, provider } = useParams<{
-    code: string;
-    state: string;
+
+  const searchParams = useSearchParams();
+
+  // Convert the URLSearchParams object to a plain object
+  const paramsObject = Object.fromEntries(searchParams.entries());
+
+  const { code, state } = paramsObject;
+
+  const { provider } = useParams<{
     provider: string;
   }>();
 
@@ -20,6 +26,7 @@ const OAuthCallbackPage = () => {
 
         try {
           // Send the code and state to your server for further processing
+          //TODO: in future we probably just push all params to the server and template them in the server
           const response = await api.auth.handleCallbackForProvider({
             provider_name: provider,
             code,
@@ -37,6 +44,9 @@ const OAuthCallbackPage = () => {
           console.error("Error handling OAuth callback:", error);
           // Handle error appropriately
         }
+      } else {
+        console.error("Missing code or state in the query parameters");
+        // Handle error appropriately
       }
     };
 
@@ -50,6 +60,7 @@ const OAuthCallbackPage = () => {
       <div>
         <p>Code: {code}</p>
         <p>State: {state}</p>
+        <p>Provider: {provider}</p>
       </div>
     </div>
   );
