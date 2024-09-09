f113a3190f6d40697d2888e05f251dfc40b9eaf6
Carl Lippert <carl.lippert@gmail.com>
2024-09-08 23:33:52 -0500

    add magic ui hero video

--- apps/marketing/src/app/(marketing)/page.tsx
+++ apps/marketing/src/app/(marketing)/page.tsx
@@ -6,6 +6,8 @@ import Faq from "@/components/LocalFaq";
 import PricingGroup from "@/components/marketing/pricing/pricing-group";
 import Image from "next/image";
 
+import HeroVideoDialog from "@repo/ui/components/magicui/hero-video-dialog";
+
 export default function IndexPage() {
   const headerList = headers();
   const referringDomain = headerList.get("referer");
@@ -21,14 +23,30 @@ export default function IndexPage() {
           Save time. Reduce errors. Automate drudgery.
         </p>
       </div>
-      <div className="mt-20 mb-20 flex flex-col items-center gap-4">
+      <div className="max-w-7xl mx-auto px-5 mt-10">
+        <HeroVideoDialog
+          className="dark:hidden block"
+          animationStyle="from-center"
+          videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
+          thumbnailSrc="https://startup-template-sage.vercel.app/hero-light.png"
+          thumbnailAlt="Hero Video"
+        />
+        {/* <HeroVideoDialog
+          className="hidden dark:block"
+          animationStyle="from-center"
+          videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
+          thumbnailSrc="https://startup-template-sage.vercel.app/hero-dark.png"
+          thumbnailAlt="Hero Video"
+        /> */}
+      </div>
+      {/* <div className="mt-20 mb-20 flex flex-col items-center gap-4">
         <div className="flex flex-col w-full px-4 items-center justify-center md:flex-row gap-4">
           {/* Add your buttons here if needed */}
-        </div>
-      </div>
-      <div className="flex flex-col w-full max-w-5xl mx-auto">
+      {/* </div> */}
+      {/* </div> */}
+      {/* <div className="flex flex-col w-full max-w-5xl mx-auto">
         <Image src={Llama} alt="Llama Screenshot" />
-      </div>
+      </div> */}
       <PricingGroup />
     </>
   );
