2b413d014e9b36d5aae8be0cd019fb455f2ec604
Carl Lippert <carl.lippert@gmail.com>
2024-09-15 17:43:40 -0500

    suspense required for search params

--- apps/web/src/app/signup/page.tsx
+++ apps/web/src/app/signup/page.tsx
@@ -1,7 +1,12 @@
+import { Suspense } from "react"; //https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout
 import LoginPage from "@/components/auth/login-component";
 
 const SignupPage = () => {
-  return <LoginPage />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <LoginPage />
+    </Suspense>
+  );
 };
 
 export default SignupPage;
