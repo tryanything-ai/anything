848ebe04eb86ee2fdfd5c79f433dbabda274dec6
Carl Lippert <carl.lippert@gmail.com>
2024-11-12 14:43:33 -0300

    rb2b

--- apps/marketing/src/app/layout.tsx
+++ apps/marketing/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { Inter as FontSans } from "next/font/google";
 import "@repo/ui/globals.css";
-
+import Script from "next/script";
 import type { Metadata } from "next";
 import { ReactNode, Suspense, JSX } from "react";
 import { siteConfig } from "../config/site";
@@ -68,6 +68,8 @@ export default function RootLayout({
       <Suspense>
         <PostHogPageview />
       </Suspense>
+      {/* RB2B Script */}
+      <Script id="reb2b-script" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `!function () {var reb2b = window.reb2b = window.reb2b || []; if (reb2b.invoked) return;reb2b.invoked = true;reb2b.methods = ["identify", "collect"]; reb2b.factory = function (method) {return function () {var args = Array.prototype.slice.call(arguments); args.unshift(method);reb2b.push(args);return reb2b;};}; for (var i = 0; i < reb2b.methods.length; i++) {var key = reb2b.methods[i];reb2b[key] = reb2b.factory(key);} reb2b.load = function (key) {var script = document.createElement("script");script.type = "text/javascript";script.async = true; script.src = "https://s3-us-west-2.amazonaws.com/b2bjsstore/b/" + key + "/9NMMZHPY38NW.js.gz"; var first = document.getElementsByTagName("script")[0]; first.parentNode.insertBefore(script, first);}; reb2b.SNIPPET_VERSION = "1.0.1";reb2b.load("9NMMZHPY38NW");}();` }} />
       <body className="bg-background text-foreground">
         <PHProvider>
           <main className="min-h-screen">{children}</main>
