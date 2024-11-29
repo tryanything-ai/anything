e4a98adfdd2329acaa20ed5efd326ec5aad27f36
Carl Lippert <carl.lippert@gmail.com>
2024-11-29 11:45:50 -0300

    prettier json editor

--- apps/web/src/components/studio/forms/fields/field-json.tsx
+++ apps/web/src/components/studio/forms/fields/field-json.tsx
@@ -86,11 +86,11 @@ export default function FieldJson({
     <div className="grid gap-3 my-2 w-full">
       <Label htmlFor={name}>{label}</Label>
       <div
-        className={cn(
-          "w-full rounded-md border border-input bg-background ring-offset-background focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2",
-          disabled && "opacity-50 cursor-not-allowed",
-          className,
-        )}
+      // className={cn(
+      //   "w-full rounded-md border border-input bg-background ring-offset-background focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2",
+      //   disabled && "opacity-50 cursor-not-allowed",
+      //   className,
+      // )}
       >
         <CodeMirror
           value={editorValue}
@@ -119,7 +119,7 @@ export default function FieldJson({
             whiteSpace: "pre",
           }}
           className={cn(
-            "w-full overflow-hidden rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+            "w-full overflow-hidden rounded-md border border-input bg-background text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
             className,
           )}
         />
