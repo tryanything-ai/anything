e98002ddb6c5975a9c2a19269141c6ef55559a56
Carl Lippert <carl.lippert@gmail.com>
2024-12-13 21:03:25 -0600

    ts issues with posthog js

--- apps/web/src/posthog/posthog.ts
+++ apps/web/src/posthog/posthog.ts
@@ -1,7 +1,7 @@
 import { PostHog } from 'posthog-node'
 
 export default function PostHogClient() {
-  const posthogClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+  const posthogClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     flushAt: 1,
     flushInterval: 0
