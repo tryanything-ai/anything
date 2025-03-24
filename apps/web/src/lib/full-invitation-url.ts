6b443fa9245e044e52ab232ad4546bef915638dc
Carl Lippert <carl.lippert@gmail.com>
2025-03-24 14:44:31 -0300

    fix invite url in app

--- apps/web/src/lib/full-invitation-url.ts
+++ apps/web/src/lib/full-invitation-url.ts
@@ -1,4 +1,4 @@
-const defaultUrl: string = process.env.NEXT_PUBLIC_HOSTED_URL || ""; 
+const defaultUrl: string = process.env.NEXT_PUBLIC_APP_URL || ""; 
 
 export default function fullInvitationUrl(token: string) {
     return `${defaultUrl}/invitation?token=${token}`;
